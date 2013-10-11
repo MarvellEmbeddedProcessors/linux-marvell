5f6251f9073d873d94a59af9b5ecdb820171e33e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-10-11 12:47:25 +0200

    ARM: mvebu: add workaround for data abort issue
    Early versions of Armada 375 SoC have a bug where the BootROM leaves
    an external data abort pending. The kernel is hit by this data abort
    as soon as it enters userspace, because it unmasks the data aborts at
    this moment. We register a custom abort handler below to ignore the
    first data abort to work around this problem.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- arch/arm/mach-mvebu/armada-375.c
+++ arch/arm/mach-mvebu/armada-375.c
@@ -26,8 +26,29 @@
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 #include <asm/mach/time.h>
+#include <asm/signal.h>
 #include "common.h"
 #include "coherency.h"
+
+/*
+ * Early versions of Armada 375 SoC have a bug where the BootROM
+ * leaves an external data abort pending. The kernel is hit by this
+ * data abort as soon as it enters userspace, because it unmasks the
+ * data aborts at this moment. We register a custom abort handler
+ * below to ignore the first data abort to work around this problem.
+ */
+static int armada_375_external_abort_wa(unsigned long addr, unsigned int fsr,
+					struct pt_regs *regs)
+{
+	static int ignore_first;
+
+	if (!ignore_first) {
+		ignore_first = 1;
+		return 0;
+	}
+
+	return 1;
+}
 
 static void __init armada_375_timer_and_clk_init(void)
 {
@@ -36,6 +57,8 @@ static void __init armada_375_timer_and_clk_init(void)
 	coherency_init();
 	BUG_ON(mvebu_mbus_dt_init(coherency_available()));
 	l2x0_of_init(0, ~0UL);
+	hook_fault_code(16 + 6, armada_375_external_abort_wa, SIGBUS, 0,
+			"imprecise external abort");
 }
 
 static const char * const armada_375_dt_compat[] = {
