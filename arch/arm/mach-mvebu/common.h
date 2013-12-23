700b135bb7743caa526f2b1eb62475527849709d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-23 12:08:05 +0100

    ARM: mvebu: remove prototypes of non-existing functions from common.h
    The mach-mvebu/common.h file contains prototypes of functions that
    have been removed, so this commit removes the corresponding
    prototypes.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- arch/arm/mach-mvebu/common.h
+++ arch/arm/mach-mvebu/common.h
@@ -17,11 +17,7 @@
 
 void mvebu_restart(char mode, const char *cmd);
 
-void armada_370_xp_init_irq(void);
-void armada_370_xp_handle_irq(struct pt_regs *regs);
-
 void armada_xp_cpu_die(unsigned int cpu);
-int armada_370_xp_coherency_init(void);
 void armada_xp_secondary_startup(void);
 extern struct smp_operations armada_xp_smp_ops;
 #endif
