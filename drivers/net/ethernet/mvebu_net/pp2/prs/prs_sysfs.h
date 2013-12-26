bb650903e8effd9096b0b905876c8741249887f9
Uri Eliyahu <uriel@marvell.com>
2013-12-26 19:07:59 +0200

    pp2: prs sysfs tree rearrange
    	- rename prsHigh directory to prs
    	- remove parse low level api to pp2/prs/debug
    
    Change-Id: I78b1befa16fae8e95ef50611740d97d4e805f870
    Signed-off-by: Uri Eliyahu <uriel@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/4898
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Dmitri Epshtein <dima@marvell.com>

---
+++ drivers/net/ethernet/mvebu_net/pp2/prs/prs_sysfs.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+Copyright (C) Marvell International Ltd. and its affiliates
+
+This software file (the "File") is owned and distributed by Marvell
+International Ltd. and/or its affiliates ("Marvell") under the following
+alternative licensing terms.  Once you have made an election to distribute the
+File under one of the following license alternatives, please (i) delete this
+introductory statement regarding license alternatives, (ii) delete the two
+license alternatives that you have not elected to use and (iii) preserve the
+Marvell copyright notice above.
+
+
+********************************************************************************
+Marvell GPL License Option
+
+If you received this File from Marvell, you may opt to use, redistribute and/or
+modify this File in accordance with the terms and conditions of the General
+Public License Version 2, June 1991 (the "GPL License"), a copy of which is
+available along with the File in the license.txt file or by writing to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 or
+on the worldwide web at http://www.gnu.org/licenses/gpl.txt.
+
+THE FILE IS DISTRIBUTED AS-IS, WITHOUT WARRANTY OF ANY KIND, AND THE IMPLIED
+WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE ARE EXPRESSLY
+DISCLAIMED.  The GPL License provides additional details about this warranty
+disclaimer.
+*******************************************************************************/
+#ifndef __prs_sysfs_h__
+#define __prs_sysfs_h__
+
+int mv_pp2_prs_low_sysfs_init(struct kobject *kobj);
+int mv_pp2_prs_low_sysfs_exit(struct kobject *kobj);
+
+#endif /* __prs_sysfs_h__*/
