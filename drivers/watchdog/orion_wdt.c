59c1c14df3db014ad6c1463b3cf54e12516ffc1b
Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
2014-02-10 20:00:26 -0300

    watchdog: orion: Remove unneeded BRIDGE_CAUSE clear
    After adding the IRQ request, the BRIDGE_CAUSE bit should be cleared by the
    bridge interrupt controller. There's no longer a need to do it in the watchdog
    driver, so we can simply remove it.
    
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Tested-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    Acked-by: Wim Van Sebroeck <wim@iguana.be>
    Tested-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    
    Conflicts:
    	drivers/watchdog/orion_wdt.c
    
    Change-Id: Idcae95176774656d6988cbae50e2ce5a4f52a095
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/15607
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>

--- drivers/watchdog/orion_wdt.c
+++ drivers/watchdog/orion_wdt.c
@@ -25,7 +25,6 @@
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/of.h>
-#include <mach/bridge-regs.h>
 
 /* RSTOUT mask register physical address for Orion5x, Kirkwood and Dove */
 #define ORION_RSTOUT_MASK_OFFSET	0x20108
@@ -61,9 +60,6 @@ static int orion_wdt_start(struct watchdog_device *wdt_dev)
 {
 	/* Set watchdog duration */
 	writel(wdt_tclk * wdt_dev->timeout, wdt_reg + WDT_VAL);
-
-	/* Clear watchdog timer interrupt */
-	writel(~WDT_INT_REQ, BRIDGE_CAUSE);
 
 	/* Enable watchdog timer */
 	atomic_io_modify(wdt_reg + TIMER_CTRL, WDT_EN, WDT_EN);
