741f93efe49a088d375739f5fb48f3d0dc973dec
Nadav Haklai <nadavh@marvell.com>
2014-02-12 11:17:37 +0200

    clk: mvebu: corediv: Add support for Armada 375 SoC
    This commit adds the support for Armada 375 SoC, introducing a new
    compatible string for the clock complex. The main difference with
    A370 is that enable/disable capability is missing from this clock
    complex on the A375 SoC.
    
    TODO: Update devicetree binding documentation.
    
    Change-Id: I86cbf6f800053da890ec770f866776847ca39ad8
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/5611
    Tested-by: Star_Automation <star@marvell.com>

--- drivers/clk/mvebu/clk-corediv.c
+++ drivers/clk/mvebu/clk-corediv.c
@@ -232,3 +232,19 @@ static void __init mvebu_corediv_clk_a370_init(struct device_node *node)
 }
 CLK_OF_DECLARE(mvebu_corediv_a370_clk, "marvell,armada-370-corediv-clock",
 	       mvebu_corediv_clk_a370_init);
+
+static void __init mvebu_corediv_clk_a375_init(struct device_node *node)
+{
+	static const struct clk_ops corediv_ops = {
+		.recalc_rate = clk_corediv_recalc_rate,
+		.round_rate = clk_corediv_round_rate,
+		.set_rate = clk_corediv_set_rate,
+	};
+
+	ratio_offset = 4;
+	ratio_reload = BIT(8);
+
+	mvebu_corediv_clk_init(node, &corediv_ops);
+}
+CLK_OF_DECLARE(mvebu_corediv_a375_clk, "marvell,armada-375-corediv-clock",
+	       mvebu_corediv_clk_a375_init);
