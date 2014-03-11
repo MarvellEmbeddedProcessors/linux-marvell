98e674fcc59583a74bdaad8a4e03e94d518ab3d2
Uri Eliyahu <uriel@marvell.com>
2014-03-11 17:29:30 +0200

    fix: alp: pp2: fix port link interrupt
    Change-Id: I65da13684020350afe04c2c2e133bd3467982117
    Signed-off-by: Uri Eliyahu <uriel@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/6316
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Dmitri Epshtein <dima@marvell.com>

--- drivers/net/ethernet/mvebu_net/pp2/hal/gmac/mvEthGmacApi.h
+++ drivers/net/ethernet/mvebu_net/pp2/hal/gmac/mvEthGmacApi.h
@@ -137,6 +137,21 @@ static INLINE MV_VOID mvGmacPortIsrUnmask(int port)
 	MV_REG_WRITE(ETH_PORT_ISR_MASK_REG(port), ETH_PORT_LINK_CHANGE_MASK);
 }
 
+static INLINE MV_VOID mvGmacPortSumIsrMask(int port)
+{
+#ifdef CONFIG_MV_ETH_PP2_1
+	MV_REG_WRITE(ETH_PORT_ISR_SUM_MASK_REG(port), 0);
+#endif
+}
+
+static INLINE MV_VOID mvGmacPortSumIsrUnmask(int port)
+{
+#ifdef CONFIG_MV_ETH_PP2_1
+	MV_REG_WRITE(ETH_PORT_ISR_SUM_MASK_REG(port), ETH_PORT_ISR_SUM_INTERN_MASK);
+#endif
+}
+
+
 void mvGmacDefaultsSet(int port);
 void mvGmacPortEnable(int port);
 void mvGmacPortDisable(int port);
