6d3b72c9f89f9a5e88638d51bb6a4eb0ba6a7f82
Jing Hua <jinghua@marvell.com>
2014-03-24 17:22:44 +0800

    alp: mv_tpm: integrate TPMv2 module into Kernel
    	add switch change that TPM teams made.
    
    Signed-off-by: Jing Hua <jinghua@marvell.com>
    
    Change-Id: Ibb68aafdfdb3d4e80a9713ca7776f9fd6c624505
    Reviewed-on: http://vgitil04.il.marvell.com:8080/6632
    Reviewed-by: Dmitri Epshtein <dima@marvell.com>
    Tested-by: Dmitri Epshtein <dima@marvell.com>

---
+++ drivers/net/ethernet/mvebu_net/switch/mv_phy.c
@@ -0,0 +1,532 @@
+/*******************************************************************************
+Copyright (C) Marvell International Ltd. and its affiliates
+
+This software file (the "File") is owned and distributed by Marvell
+International Ltd. and/or its affiliates ("Marvell") under the following
+alternative licensing terms.  Once you have made an election to distribute the
+File under one of the following license alternatives, please (i) delete this
+introductory statement regarding license alternatives, (ii) delete the two
+license alternatives that you have not elected to use and (iii) preserve the
+Marvell copyright notice above.
+
+********************************************************************************
+Marvell GPL License Option
+
+If you received this File from Marvell, you may opt to use, redistribute and/or
+modify this File in accordance with the terms and conditions of the General
+Public License Version 2, June 1991 (the "GPL License"), a copy of which is
+available along with the File in the license.txt file or by writing to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 or
+on the worldwide web at http://www.gnu.org/licenses/gpl.txt.
+
+THE FILE IS DISTRIBUTED AS-IS, WITHOUT WARRANTY OF ANY KIND, AND THE IMPLIED
+WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE ARE EXPRESSLY
+DISCLAIMED.  The GPL License provides additional details about this warranty
+disclaimer.
+*******************************************************************************/
+
+#include <linux/etherdevice.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/mv_switch.h>
+#include <linux/module.h>
+
+#include "mvOs.h"
+#include "mvSysHwConfig.h"
+#include "eth-phy/mvEthPhy.h"
+#ifdef MV_INCLUDE_ETH_COMPLEX
+#include "ctrlEnv/mvCtrlEthCompLib.h"
+#endif /* MV_INCLUDE_ETH_COMPLEX */
+
+#include "msApi.h"
+#include "mv_switch.h"
+#include "mv_phy.h"
+#include "mv_mux/mv_mux_netdev.h"
+
+/*******************************************************************************
+* mv_phy_port_power_state_set
+*
+* DESCRIPTION:
+*	The API configures the PHY port state of given switch logical port.
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*	state  - PHY port power state to set.
+*			GT_TRUE: power on
+*			GT_FALSE: power down
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*******************************************************************************/
+int mv_phy_port_power_state_set(unsigned int lport, GT_BOOL state)
+{
+	GT_BOOL power_state;
+	GT_BOOL pre_power_state;
+	GT_STATUS rc = GT_OK;
+
+	if (state == GT_TRUE)
+		power_state = GT_FALSE;
+	else
+		power_state = GT_TRUE;
+
+	/* get the current link status */
+	rc = gprtGetPortPowerDown(mv_switch_qd_dev_get(), lport, &pre_power_state);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPortPowerDown()\n");
+
+	rc = gprtPortPowerDown(mv_switch_qd_dev_get(), lport, power_state);
+	SW_IF_ERROR_STR(rc, "failed to call gprtPortPowerDown()\n");
+
+	/* since link change event from HW (via interrupt) does not happen
+	   for UP->DOWN link change, only for DOWN->UP after link negotiation,
+	   print a port Down state change for this use case */
+	if (pre_power_state == GT_FALSE && power_state == GT_TRUE)
+		pr_err("Port %d: Link-down\n", lport);
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_power_state_get
+*
+* DESCRIPTION:
+*	The API gets the PHY port state of given switch logical port.
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	state  - PHY port power state to set.
+*			GT_TRUE: power on
+*			GT_FALSE: power down
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*******************************************************************************/
+int mv_phy_port_power_state_get(unsigned int lport, GT_BOOL *state)
+{
+	GT_BOOL power_state;
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtGetPortPowerDown(mv_switch_qd_dev_get(), lport, &power_state);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPortPowerDown()\n");
+
+	if (power_state == GT_TRUE)
+		*state = GT_FALSE;
+	else
+		*state = GT_TRUE;
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_autoneg_mode_set
+*
+* DESCRIPTION:
+*	The API configures the auto negotiation state of given switch logical port.
+* INPUTS:
+*	lport          - logical switch PHY port ID.
+*	autoneg_state  - autonegotiation state, enabled or disabled.
+*	autoneg_mode   - enum:
+*			SPEED_AUTO_DUPLEX_AUTO: Auto for both speed and duplex
+*			SPEED_1000_DUPLEX_AUTO: 1000Mbps and auto duplex
+*			SPEED_100_DUPLEX_AUTO:  100Mbps and auto duplex
+*			SPEED_10_DUPLEX_AUTO:   10Mbps and auto duplex
+*			SPEED_AUTO_DUPLEX_FULL: Auto for speed only and Full duplex
+*			SPEED_AUTO_DUPLEX_HALF: Auto for speed only and Half duplex. (1000Mbps is not supported)
+*			SPEED_1000_DUPLEX_FULL: 1000Mbps Full duplex.
+*			SPEED_1000_DUPLEX_HALF: 1000Mbps half duplex.
+*			SPEED_100_DUPLEX_FULL:  100Mbps Full duplex.
+*			SPEED_100_DUPLEX_HALF:  100Mbps half duplex.
+*			SPEED_10_DUPLEX_FULL:   10Mbps Full duplex.
+*			SPEED_10_DUPLEX_HALF:   10Mbps half duplex.
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*******************************************************************************/
+int mv_phy_port_autoneg_mode_set(unsigned int lport, GT_BOOL autoneg_state, GT_PHY_AUTO_MODE autoneg_mode)
+{
+	GT_STATUS rc = GT_OK;
+
+	if (GT_FALSE == autoneg_state) {
+		rc = gprtPortAutoNegEnable(mv_switch_qd_dev_get(), lport, GT_FALSE);
+		SW_IF_ERROR_STR(rc, "failed to call gprtPortAutoNegEnable()\n");
+
+		return MV_OK;
+	} else	{
+		rc = gprtSetPortAutoMode(mv_switch_qd_dev_get(), lport, autoneg_mode);
+		SW_IF_ERROR_STR(rc, "failed to call gprtSetPortAutoMode()\n");
+
+		rc = gprtPortAutoNegEnable(mv_switch_qd_dev_get(), lport, GT_TRUE);
+		SW_IF_ERROR_STR(rc, "failed to call gprtPortAutoNegEnable()\n");
+	}
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_autoneg_mode_get
+*
+* DESCRIPTION:
+*	The API gets the auto negotiation state of given switch logical port.
+* INPUTS:
+*	lport          - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	autoneg_state  - autonegotiation state, enabled or disabled.
+*	autoneg_mode   - enum:
+*			SPEED_AUTO_DUPLEX_AUTO: Auto for both speed and duplex
+*			SPEED_1000_DUPLEX_AUTO: 1000Mbps and auto duplex
+*			SPEED_100_DUPLEX_AUTO:  100Mbps and auto duplex
+*			SPEED_10_DUPLEX_AUTO:   10Mbps and auto duplex
+*			SPEED_AUTO_DUPLEX_FULL: Auto for speed only and Full duplex
+*			SPEED_AUTO_DUPLEX_HALF: Auto for speed only and Half duplex. (1000Mbps is not supported)
+*			SPEED_1000_DUPLEX_FULL: 1000Mbps Full duplex.
+*			SPEED_1000_DUPLEX_HALF: 1000Mbps half duplex.
+*			SPEED_100_DUPLEX_FULL:  100Mbps Full duplex.
+*			SPEED_100_DUPLEX_HALF:  100Mbps half duplex.
+*			SPEED_10_DUPLEX_FULL:   10Mbps Full duplex.
+*			SPEED_10_DUPLEX_HALF:   10Mbps half duplex.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*******************************************************************************/
+int mv_phy_port_autoneg_mode_get(unsigned int lport, GT_BOOL *autoneg_state, GT_PHY_AUTO_MODE *autoneg_mode)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtGetPortAutoNegState(mv_switch_qd_dev_get(), lport, autoneg_state);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPortAutoNegState()\n");
+
+	rc = gprtGetPortAutoMode(mv_switch_qd_dev_get(), lport, autoneg_mode);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPortAutoMode()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_autoneg_restart
+*
+* DESCRIPTION:
+*	The API restarts the auto negotiation of given switch logical port.
+* INPUTS:
+*	lport - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*******************************************************************************/
+int mv_phy_port_autoneg_restart(unsigned int lport)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtPortRestartAutoNeg(mv_switch_qd_dev_get(), lport);
+	SW_IF_ERROR_STR(rc, "failed to call gprtPortRestartAutoNeg()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_pause_mode_set
+*
+* DESCRIPTION:
+*	This routine will set the pause bit in Autonegotiation Advertisement
+*	Register. And restart the autonegotiation.
+*
+* INPUTS:
+*	lport - logical switch PHY port ID.	The physical address, if SERDES device is accessed
+*	state - GT_PHY_PAUSE_MODE enum value.
+*		GT_PHY_NO_PAUSE		- disable pause
+*		GT_PHY_PAUSE		- support pause
+*		GT_PHY_ASYMMETRIC_PAUSE	- support asymmetric pause
+*		GT_PHY_BOTH_PAUSE	- support both pause and asymmetric pause
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+* COMMENTS:
+*	Data sheet register 4.10 Autonegotiation Advertisement Register
+*******************************************************************************/
+int mv_phy_port_pause_mode_set(unsigned int lport, GT_PHY_PAUSE_MODE state)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtSetPause(mv_switch_qd_dev_get(), lport, state);
+	SW_IF_ERROR_STR(rc, "failed to call gprtSetPause()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_pause_mode_get
+*
+* DESCRIPTION:
+*	This routine will get the pause bit in Autonegotiation Advertisement
+*	Register.
+*
+* INPUTS:
+*	lport - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	state - GT_PHY_PAUSE_MODE enum value.
+*		GT_PHY_NO_PAUSE		- disable pause
+*		GT_PHY_PAUSE		- support pause
+*		GT_PHY_ASYMMETRIC_PAUSE	- support asymmetric pause
+*		GT_PHY_BOTH_PAUSE	- support both pause and asymmetric pause
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+* COMMENTS:
+*	Data sheet register 4.10 Autonegotiation Advertisement Register
+*******************************************************************************/
+int mv_phy_port_pause_mode_get(unsigned int lport, GT_PHY_PAUSE_MODE *state)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtGetPause(mv_switch_qd_dev_get(), lport, state);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPause()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_pause_state_get
+*
+* DESCRIPTION:
+*	This routine will get the current pause state.
+*	Register.
+*
+* INPUTS:
+*	lport - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	state - pause state
+*		GT_FALSE: MAC pause is not implemented
+*		GT_TRUE: MAC pause is implemented
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*******************************************************************************/
+int mv_phy_port_pause_state_get(unsigned int lport, GT_BOOL *state)
+{
+	GT_BOOL force;
+	GT_BOOL pause;
+	GT_STATUS rc = GT_OK;
+
+	rc = gpcsGetForcedFC(mv_switch_qd_dev_get(), lport, &force);
+	SW_IF_ERROR_STR(rc, "failed to call gpcsGetForcedFC()\n");
+
+	if (force) {
+		rc = gpcsGetFCValue(mv_switch_qd_dev_get(), lport, &pause);
+		SW_IF_ERROR_STR(rc, "failed to call gpcsGetFCValue()\n");
+	} else {
+		rc = gprtGetPauseEn(mv_switch_qd_dev_get(), lport, &pause);
+		SW_IF_ERROR_STR(rc, "failed to call gprtGetPauseEn()\n");
+	}
+
+	*state = pause;
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_egr_loopback_set
+*
+* DESCRIPTION:
+*	Enable/Disable egress loopback of switch PHY port,
+*       and enable/disable force link port.
+*
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*	enable - enable/disable egree loopback.
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*
+* COMMENTS:
+*	Data sheet register 0.14 - Loop_back
+*******************************************************************************/
+int mv_phy_port_egr_loopback_set(unsigned int lport, GT_BOOL enable)
+{
+	GT_STATUS rc = GT_OK;
+	GT_BOOL link_forced;
+
+	rc = gprtSetPortLoopback(mv_switch_qd_dev_get(), lport, enable);
+	SW_IF_ERROR_STR(rc, "failed to call gprtSetPortLoopback()\n");
+
+	/* Get port force link statue */
+	rc = gpcsGetForcedLink(mv_switch_qd_dev_get(), lport, &link_forced);
+	SW_IF_ERROR_STR(rc, "failed to call gpcsGetForcedLink()\n");
+	if (((enable == GT_TRUE) && (link_forced == GT_FALSE)) ||
+	    ((enable == GT_FALSE) && (link_forced == GT_TRUE))) {
+		/* Set force link */
+		rc = gpcsSetForcedLink(mv_switch_qd_dev_get(), lport, enable);
+		SW_IF_ERROR_STR(rc, "failed to call gpcsSetForcedLink()\n");
+		/* Set force link value */
+		rc = gpcsSetLinkValue(mv_switch_qd_dev_get(), lport, enable);
+		SW_IF_ERROR_STR(rc, "failed to call gpcsSetLinkValue()\n");
+	}
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_egr_loopback_get
+*
+* DESCRIPTION:
+*	This API get enabled/disabled state of egress loopback of switch PHY port.
+*
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	enable - enable/disable egress loopback.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*
+* COMMENTS:
+*	Data sheet register 0.14 - Loop_back
+*******************************************************************************/
+int mv_phy_port_egr_loopback_get(unsigned int lport, GT_BOOL *enable)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtGetPortLoopback(mv_switch_qd_dev_get(), lport, enable);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPortLoopback()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_ingr_loopback_set
+*
+* DESCRIPTION:
+*	This API sets enabled/disabled state of ingress loopback of switch PHY port.
+*
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*	enable - enable/disable ingress loopback.
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*
+* COMMENTS:
+*	Data sheet register FE:28.4, GE:21_2.14  - Loop_back
+********************************************************************************/
+int mv_phy_port_ingr_loopback_set(unsigned int lport, GT_BOOL enable)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtSetPortLineLoopback(mv_switch_qd_dev_get(), lport, enable);
+	SW_IF_ERROR_STR(rc, "failed to call gprtSetPortLineLoopback()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_ingr_loopback_get
+*
+* DESCRIPTION:
+*	This API gets enabled/disabled state of ingress loopback of switch PHY port.
+*
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*
+* OUTPUTS:
+*	enable - enable/disable ingress loopback.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+*
+* COMMENTS:
+*	Data sheet register FE:28.4, GE:21_2.14  - Loop_back
+********************************************************************************/
+int mv_phy_port_ingr_loopback_get(unsigned int lport, GT_BOOL *enable)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtGetPortLineLoopback(mv_switch_qd_dev_get(), lport, enable);
+	SW_IF_ERROR_STR(rc, "failed to call gprtGetPortLineLoopback()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_speed_set
+*
+* DESCRIPTION:
+*	This API sets the speed of switch PHY port.
+*
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*	enable - enable/disable ingress loopback.
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+********************************************************************************/
+int mv_phy_port_speed_set(unsigned int lport, GT_PHY_SPEED speed)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtSetPortSpeed(mv_switch_qd_dev_get(), lport, speed);
+	SW_IF_ERROR_STR(rc, "failed to call gprtSetPortSpeed()\n");
+
+	return MV_OK;
+}
+
+/*******************************************************************************
+* mv_phy_port_duplex_set
+*
+* DESCRIPTION:
+*	This API sets the duplex mode of switch PHY port.
+*
+* INPUTS:
+*	lport  - logical switch PHY port ID.
+*	mode   - enable or disable duplex mode.
+*
+* OUTPUTS:
+*	None.
+*
+* RETURNS:
+*	On success return MV_OK.
+*	On error different types are returned according to the case.
+********************************************************************************/
+int mv_phy_port_duplex_set(unsigned int lport, GT_BOOL mode)
+{
+	GT_STATUS rc = GT_OK;
+
+	rc = gprtSetPortDuplexMode(mv_switch_qd_dev_get(), lport, mode);
+	SW_IF_ERROR_STR(rc, "failed to call gprtSetPortDuplexMode()\n");
+
+	return MV_OK;
+}
--- drivers/net/ethernet/mvebu_net/switch/mv_switch.h
+++ drivers/net/ethernet/mvebu_net/switch/mv_switch.h
@@ -29,9 +29,23 @@ disclaimer.
 #define __mv_switch_h__
 
 #include "mv802_3.h"
+#ifdef CONFIG_MV_INCLUDE_SWITCH
+#include "msApiTypes.h"
+#include "msApiDefs.h"
+#endif
 #include "mv_mux_netdev.h"
 
 #define MV_SWITCH_DB_NUM			16
+
+/*TPM start*/
+#define MV_SWITCH_PORT_NOT_BELONG		(0)	/* VLAN does not belong to port		*/
+#define MV_SWITCH_PORT_BELONG			(1)	/* VLAN belong to port			*/
+#define MV_SWITCH_MAX_VLAN_NUM			(4096)	/* Maximum switch VLAN number		*/
+#define MV_SWITCH_MAX_PORT_NUM			(7)	/* Maximum switch port number		*/
+#define MV_SWITCH_MAX_QUEUE_NUM			(4)	/* Maximum switch queue number per port	*/
+#define MV_SWITCH_CPU_PORT_NUM			(6)	/* Switch port connected to CPU		*/
+#define MV_SWITCH_DEFAULT_WEIGHT		(2)	/* Switch default queue weight		*/
+/*TPM end*/
 
 #define MV_SWITCH_PHY_ACCESS			1
 #define MV_SWITCH_PORT_ACCESS			2
@@ -43,6 +57,66 @@ disclaimer.
 #define MV_SWITCH_GROUP_VLAN_ID(grp)       (((grp) + 1) << 8)
 #define MV_SWITCH_VLAN_TO_GROUP(vid)       ((((vid) & 0xf00) >> 8) - 1)
 
+#define MV_SWITCH_PIRL_BKTTYPR_UNKNOWN_MULTICAST_BIT    1
+#define MV_SWITCH_PIRL_BKTTYPR_BROADCAST_BIT		2
+#define MV_SWITCH_PIRL_BKTTYPR_MULTICAST_BIT		3
+
+#define MV_SWITCH_PIRL_RESOURCE_BROADCAST    1
+#define MV_SWITCH_PIRL_RESOURCE_MULTICAST    2
+
+
+#ifdef CONFIG_MV_INCLUDE_SWITCH
+/*TPM start*/
+#define SW_IF_NULL(ptr) { \
+	if (ptr == NULL) {\
+		pr_err("(error) %s(%d) recvd NULL pointer\n", __func__, __LINE__);\
+		return MV_FAIL;\
+	} \
+}
+#define SW_IF_ERROR_STR(rc, format, ...) { \
+	if (rc) {\
+		pr_err("(error) %s(%d) (rc=%d): "format, __func__, __LINE__, rc, ##__VA_ARGS__);\
+		return MV_FAIL;\
+	} \
+}
+
+/* port mirror direction */
+enum sw_mirror_mode_t {
+	MV_SWITCH_MIRROR_INGRESS,
+	MV_SWITCH_MIRROR_EGRESS,
+	MV_SWITCH_MIRROR_BOTH
+};
+
+
+/* operations requested by switch device from mux device */
+struct mux_device_ops {
+	int	(*update_link)(void *cookie, int link_up);
+};
+
+/* port mirror direction */
+enum sw_mac_addr_type_t {
+	MV_SWITCH_ALL_MAC_ADDR,
+	MV_SWITCH_UNICAST_MAC_ADDR,
+	MV_SWITCH_MULTICAST_MAC_ADDR,
+	MV_SWITCH_DYNAMIC_MAC_ADDR,
+	MV_SWITCH_STATIC_MAC_ADDR
+};
+
+/* logical port VLAN information */
+struct sw_port_info_t {
+	GT_DOT1Q_MODE	port_mode;
+	unsigned int	vlan_blong[MV_SWITCH_MAX_VLAN_NUM];
+};
+
+/* VLAN information */
+struct sw_vlan_info_t {
+	unsigned int	port_bm;				/* bitmap of the ports in this VLAN */
+	unsigned char	egr_mode[MV_SWITCH_MAX_PORT_NUM];	/* egress mode of each port         */
+	GT_VTU_ENTRY	vtu_entry;				/* Add this member to record HW VT info to SW table */
+};
+/*TPM end*/
+#endif
+/*unsigned int	mv_switch_link_detection_init(struct mv_switch_pdata *plat_data);*/
 void mv_switch_interrupt_mask(void);
 void mv_switch_interrupt_unmask(void);
 void mv_switch_interrupt_clear(void);
@@ -56,6 +130,7 @@ int     mv_switch_port_based_vlan_set(unsigned int ports_mask, int set_cpu_port)
 int     mv_switch_vlan_in_vtu_set(unsigned short vlan_id, unsigned short db_num, unsigned int ports_mask);
 int     mv_switch_atu_db_flush(int db_num);
 int     mv_switch_vlan_set(u16 vlan_grp_id, u16 port_map);
+int mv_switch_promisc_set(int db, u8 promisc_on);
 
 int     mv_switch_reg_read(int port, int reg, int type, MV_U16 *value);
 int     mv_switch_reg_write(int port, int reg, int type, MV_U16 value);
@@ -78,7 +153,102 @@ int		mv_switch_group_enable(int db);
 int		mv_switch_group_disable(int db);
 int		mv_switch_link_status_get(int db);
 int		mv_switch_group_cookie_set(int db, void *cookie);
+int		mv_switch_mac_update(int db, unsigned char *old_mac, unsigned char *new_mac);
 int		mv_switch_mac_addr_set(int db, unsigned char *mac_addr, unsigned char op);
 int		mv_switch_mux_ops_set(const struct mv_switch_mux_ops *mux_ops_ptr);
-int		mv_switch_promisc_set(int db, u8 promisc_on);
+
+#ifdef CONFIG_MV_INCLUDE_SWITCH
+/*TPM start*/
+int mv_switch_mac_update(int db, unsigned char *old_mac, unsigned char *new_mac);
+int mv_switch_port_discard_tag_set(unsigned int lport, GT_BOOL mode);
+int mv_switch_port_discard_tag_get(unsigned int lport, GT_BOOL *mode);
+int mv_switch_port_discard_untag_set(unsigned int lport, GT_BOOL mode);
+int mv_switch_port_discard_untag_get(unsigned int lport, GT_BOOL *mode);
+int mv_switch_port_def_vid_set(unsigned int lport, unsigned short vid);
+int mv_switch_port_def_vid_get(unsigned int lport, unsigned short *vid);
+int mv_switch_port_def_pri_set(unsigned int lport, unsigned char pri);
+int mv_switch_port_def_pri_get(unsigned int lport, unsigned char *pri);
+int mv_switch_port_vid_add(unsigned int lport, unsigned short vid, unsigned char egr_mode, bool belong);
+int mv_switch_port_vid_del(unsigned int lport, unsigned short vid);
+int mv_switch_vid_get(unsigned int vid, GT_VTU_ENTRY *vtu_entry, unsigned int *found);
+int mv_switch_port_vid_egress_mode_set(unsigned int lport, unsigned short vid, unsigned char egr_mode);
+int mv_switch_unknown_unicast_flood_set(unsigned char lport, GT_BOOL enable);
+int mv_switch_unknown_unicast_flood_get(unsigned char lport, GT_BOOL *enable);
+int mv_switch_unknown_multicast_flood_set(unsigned char lport, GT_BOOL enable);
+int mv_switch_unknown_multicast_flood_get(unsigned char lport, GT_BOOL *enable);
+int mv_switch_broadcast_flood_set(GT_BOOL enable);
+int mv_switch_broadcast_flood_get(GT_BOOL *enable);
+int mv_switch_port_count3_get(unsigned int lport, GT_STATS_COUNTER_SET3 *count);
+int mv_switch_port_drop_count_get(unsigned int lport, GT_PORT_STAT2 *count);
+int mv_switch_port_count_clear(unsigned int lport);
+int mv_switch_count_clear(void);
+int mv_switch_ingr_limit_mode_set(unsigned int lport, GT_RATE_LIMIT_MODE mode);
+int mv_switch_ingr_limit_mode_get(unsigned int lport, GT_RATE_LIMIT_MODE *mode);
+int mv_switch_ingr_police_rate_set(unsigned int	lport,
+					GT_PIRL2_COUNT_MODE	count_mode,
+					unsigned int		cir);
+int mv_switch_ingr_police_rate_get(unsigned int		lport,
+				   GT_PIRL2_COUNT_MODE	*count_mode,
+				   unsigned int		*cir);
+int mv_switch_egr_rate_limit_set(unsigned int lport, GT_PIRL_ELIMIT_MODE mode, unsigned int rate);
+int mv_switch_egr_rate_limit_get(unsigned int lport, GT_PIRL_ELIMIT_MODE *mode, unsigned int *rate);
+int mv_switch_ingr_broadcast_rate_set(unsigned int lport, GT_PIRL2_COUNT_MODE count_mode, unsigned int cir);
+int mv_switch_ingr_broadcast_rate_get(unsigned int lport, GT_PIRL2_COUNT_MODE *count_mode, unsigned int *cir);
+int mv_switch_ingr_multicast_rate_set(unsigned int lport, GT_PIRL2_COUNT_MODE count_mode, unsigned int cir);
+int mv_switch_ingr_multicast_rate_get(unsigned int lport, GT_PIRL2_COUNT_MODE *count_mode, unsigned int *cir);
+int mv_switch_port_mirror_set(unsigned int sport, enum sw_mirror_mode_t mode, GT_BOOL enable, unsigned int dport);
+int mv_switch_port_mirror_get(unsigned int sport, enum sw_mirror_mode_t mode, GT_BOOL *enable, unsigned int *dport);
+int mv_switch_age_time_set(unsigned int time);
+int mv_switch_age_time_get(unsigned int *time);
+int mv_switch_mac_learn_disable_set(unsigned int lport, GT_BOOL enable);
+int mv_switch_mac_learn_disable_get(unsigned int lport, GT_BOOL *enable);
+int mv_switch_learn2all_enable_set(GT_BOOL enable);
+int mv_switch_learn2all_enable_get(GT_BOOL *enable);
+int mv_switch_mac_limit_set(unsigned int lport, unsigned int mac_num);
+int mv_switch_mac_limit_get(unsigned int lport, unsigned int *mac_num);
+int mv_switch_mac_addr_add(unsigned int port_bm, unsigned char mac_addr[6], unsigned int mode);
+int mv_switch_mac_addr_del(unsigned int lport, unsigned char mac_addr[6]);
+int mv_switch_port_qos_mode_set(unsigned int lport, GT_PORT_SCHED_MODE mode);
+int mv_switch_port_qos_mode_get(unsigned int lport, GT_PORT_SCHED_MODE *mode);
+int mv_switch_queue_weight_set(unsigned int lport, unsigned char queue, unsigned char weight);
+int mv_switch_queue_weight_get(unsigned int lport, unsigned char queue, unsigned char *weight);
+int mv_switch_mtu_set(unsigned int mtu);
+int mv_switch_mtu_get(unsigned int *mtu);
+int mv_switch_link_state_get(unsigned int lport, GT_BOOL *state);
+int mv_switch_duplex_state_get(unsigned int lport, GT_BOOL *state);
+int mv_switch_speed_state_get(unsigned int lport, GT_PORT_SPEED_MODE *speed);
+int mv_switch_port_vlan_filter_set(unsigned int lport, unsigned char filter);
+int mv_switch_port_vlan_filter_get(unsigned int lport, unsigned char *filter);
+int mv_switch_port_vlan_mode_set(unsigned int lport, GT_DOT1Q_MODE mode);
+int mv_switch_port_vlan_mode_get(unsigned int lport, GT_DOT1Q_MODE *mode);
+int mv_switch_port_mac_filter_mode_set(unsigned int lport, GT_SA_FILTERING mode);
+int mv_switch_port_mac_filter_mode_get(unsigned int lport, GT_SA_FILTERING *mode);
+int mv_switch_port_mac_filter_entry_add(unsigned int lport, unsigned char *mac,
+	unsigned short vlan, GT_SA_FILTERING mode);
+int mv_switch_port_mac_filter_entry_del(unsigned int lport, unsigned char *mac,
+	unsigned short	vlan, GT_SA_FILTERING mode);
+int mv_switch_port_vlan_set(unsigned int lport, GT_LPORT mem_port[], unsigned int mem_num);
+int mv_switch_port_vlan_get(unsigned int lport, GT_LPORT mem_port[], unsigned int *mem_num);
+int mv_switch_mh_mode_set(unsigned char lport, GT_BOOL enable);
+int mv_switch_mh_mode_get(unsigned char lport, GT_BOOL *enable);
+int mv_switch_frame_mode_set(unsigned char lport, GT_FRAME_MODE mode);
+int mv_switch_frame_mode_get(unsigned char lport, GT_FRAME_MODE *mode);
+int mv_switch_etype_set(unsigned char lport, unsigned short etype);
+int mv_switch_etype_get(unsigned char lport, unsigned short *etype);
+int mv_switch_port_preamble_set(unsigned char lport, unsigned short preamble);
+int mv_switch_port_preamble_get(unsigned char lport, unsigned short *preamble);
+int mv_switch_port_force_speed_set(unsigned int lport, GT_BOOL enable, unsigned int mode);
+int mv_switch_port_force_speed_get(unsigned int lport, GT_BOOL *enable, unsigned int *mode);
+int mv_switch_port_force_duplex_set(unsigned int lport, GT_BOOL enable, GT_BOOL value);
+int mv_switch_port_force_duplex_get(unsigned int lport, GT_BOOL *enable, GT_BOOL *value);
+int mv_switch_atu_next_entry_get(GT_ATU_ENTRY *atu_entry);
+int mv_switch_vtu_flush(void);
+int mv_switch_atu_flush(GT_FLUSH_CMD flush_cmd, unsigned short db_num);
+unsigned int mv_switch_port_num_get(void);
+GT_QD_DEV *mv_switch_qd_dev_get(void);
+int mv_switch_vtu_shadow_dump(void);
+int mv_switch_vlan_tunnel_set(unsigned int lport, GT_BOOL mode);
+int mv_switch_cpu_port_get(unsigned int *cpu_port);
+/*TPM end*/
+#endif
 #endif /* __mv_switch_h__ */
