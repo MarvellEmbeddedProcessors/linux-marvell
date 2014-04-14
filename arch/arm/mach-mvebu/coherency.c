c9723718c942fcf2ec9cf3f9d61a623350bf8f6e
Gregory CLEMENT <gregory.clement@free-electrons.com>
2014-04-14 17:10:05 +0200

    ARM: mvebu: remove the address parameter for ll_set_cpu_coherent
    In order to be able to deal with the MMU enabled and the MMU disabled
    cases, the base address of the coherency registers was passed to the
    function. The address by itself was not interesting as it can't change
    for a given SoC, the only thing we need is to have a distinction
    between the physical or the virtual address.
    
    This patch add a check of the MMU bit to choose the accurate address,
    then the calling function doesn't have to pass this information.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Link: https://lkml.kernel.org/r/1397488214-20685-3-git-send-email-gregory.clement@free-electrons.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    
    Conflicts:
    
    	arch/arm/mach-mvebu/coherency.c
    	arch/arm/mach-mvebu/coherency_ll.S
    	arch/arm/mach-mvebu/headsmp.S
    
    Change-Id: I25a8605ab3e116ee3e927d91bce7019de4603c40
    Reviewed-on: http://vgitil04.il.marvell.com:8080/9159
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Nadav Haklai <nadavh@marvell.com>

--- arch/arm/mach-mvebu/coherency.c
+++ arch/arm/mach-mvebu/coherency.c
@@ -29,8 +29,9 @@
 #include <asm/cacheflush.h>
 #include "armada-370-xp.h"
 
-unsigned long __cpuinitdata coherency_phys_base;
-static void __iomem *coherency_base;
+
+unsigned long coherency_phys_base;
+void __iomem *coherency_base;
 static void __iomem *coherency_cpu_base;
 
 /* Coherency fabric registers */
@@ -63,7 +64,7 @@ static struct of_device_id of_coherency_table[] = {
 };
 
 /* Function defined in coherency_ll.S */
-int ll_set_cpu_coherent(void __iomem *base_addr, unsigned int hw_cpu_id);
+int ll_set_cpu_coherent(unsigned int hw_cpu_id);
 
 int set_cpu_coherent(unsigned int hw_cpu_id, int smp_group_id)
 {
@@ -73,7 +74,7 @@ int set_cpu_coherent(unsigned int hw_cpu_id, int smp_group_id)
 		return 1;
 	}
 
-	return ll_set_cpu_coherent(coherency_base, hw_cpu_id);
+	return ll_set_cpu_coherent(hw_cpu_id);
 }
 
 static inline void mvebu_hwcc_sync_io_barrier(void)
