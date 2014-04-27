888d4f5437453806a7631896abe459a3c5c7722e
Nadav Haklai <nadavh@marvell.com>
2014-04-27 11:43:55 +0300

    ARM: kernel: allow the SCU to be enabled even on !SMP
    Certain non-SMP platforms rely on the SCU being enabled to provide hardware I/O
    coherency between DMA masters and the CPU. Therefore, the scu_enable() hook should
    not be reduced to a no-op when CONFIG_SMP is disabled.
    
    Change-Id: I3fcd89365da373010f124a905889056ea298c89b
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/7720
    Tested-by: Star_Automation <star@marvell.com>

--- arch/arm/kernel/smp_scu.c
+++ arch/arm/kernel/smp_scu.c
@@ -22,7 +22,6 @@
 #define SCU_INVALIDATE		0x0c
 #define SCU_FPGA_REVISION	0x10
 
-#ifdef CONFIG_SMP
 /*
  * Get the number of CPU cores from the SCU configuration
  */
@@ -62,7 +61,6 @@ void scu_enable(void __iomem *scu_base)
 	 */
 	flush_cache_all();
 }
-#endif
 
 /*
  * Set the executing CPUs power mode as defined.  This will be in
