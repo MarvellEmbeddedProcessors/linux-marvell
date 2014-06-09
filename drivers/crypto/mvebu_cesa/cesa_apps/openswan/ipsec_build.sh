3ff0f9b67eeaa62bbbd9a99f77e4e8dec7285ac3
Nadav Haklai <nadavh@marvell.com>
2014-06-09 14:30:36 +0300

    cesa: Add support for LKv3.10 to Openswan.
    	- Update Openswan to latest version - 2.6.41
    	- Import LKv3.10 support from Libreswan 3.8
    	- Adjust scripts and readme file
    
    Change-Id: Ieec2171c73a634e6c2644bb7080b506bdc202590
    Signed-off-by: Michal Mazur <arg@semihalf.com>
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/8430

--- drivers/crypto/mvebu_cesa/cesa_apps/openswan/ipsec_build.sh
+++ drivers/crypto/mvebu_cesa/cesa_apps/openswan/ipsec_build.sh
@@ -1,18 +1,24 @@
+#!/bin/bash
 # IPSEC Module build script
+set -e
 
 # Optionaly download the sources from web
-#wget http://download.openswan.org/openswan/openswan-2.6.37.tar.gz
+#wget http://download.openswan.org/openswan/openswan-2.6.41.tar.gz
 
 # Extract sources localy
-tar -zxvf openswan-2.6.37.tar.gz
-cd openswan-2.6.37
+tar -zxvf openswan-2.6.41.tar.gz
+cd openswan-2.6.41
 
 # Apply marvell patch over openswan sources
-patch -p1 < ../0001-mv_openswan_2_6_37.patch
+patch -p1 < ../mv_openswan_2_6_41.patch
+# Add support for LKv3.10 (imported from libreswan 3.8)
+patch -p1 < ../lk_3_10_support_openswan_2_6_41.patch
 
 # Build ipsec module
-make KERNELSRC=../../../ module ARCH=arm
+make KERNELSRC=../../../../../../ module ARCH=arm
 
 # Copy to modules output directory
-mkdir -p ${1}/kernel/ipsec
-cp modobj26/ipsec.ko ${1}/kernel/ipsec
+if [ "$1" != "" ]; then
+	mkdir -p ${1}/kernel/ipsec
+	cp modobj26/ipsec.ko ${1}/kernel/ipsec
+fi
