62d1db4144f2e82209b110ac2e7d08cdd86be746
Nadav Haklai <nadavh@marvell.com>
2014-08-07 14:37:08 +0300

    Bump LSP release to v3.10.39 2014_T3.0.eng_drop_v1
    Change-Id: I7ca9965dbcc645237fb9d547d8b9094d4f9f97cb
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/9921
    Tested-by: Star_Automation <star@marvell.com>

--- arch/arm/mach-mvebu/common.h
+++ arch/arm/mach-mvebu/common.h
@@ -15,7 +15,7 @@
 #ifndef __ARCH_MVEBU_COMMON_H
 #define __ARCH_MVEBU_COMMON_H
 
-#define LSP_VERSION    "linux-3.10.39-2014_T2.0"
+#define LSP_VERSION    "linux-3.10.39-2014_T3.0.eng_drop_v1"
 
 void mvebu_restart(char mode, const char *cmd);
 
