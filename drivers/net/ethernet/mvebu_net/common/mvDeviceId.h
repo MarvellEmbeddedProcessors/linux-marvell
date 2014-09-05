99da8cb45d7a660b52a3710fda5747705cfd7579
Hanna Hawa <hannah@marvell.com>
2014-09-05 21:14:13 +0300

    a39x: add missing Model IDs for A39x SoC
            - Add missing model IDs for A39x SoC: MV_6928 and MV_6910
    
    Change-Id: I4cc263a51e29b4c15abce101eef16c45bee789fa
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/11508
    Reviewed-by: Shadi Ammouri <shadi@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- drivers/net/ethernet/mvebu_net/common/mvDeviceId.h
+++ drivers/net/ethernet/mvebu_net/common/mvDeviceId.h
@@ -401,7 +401,9 @@ extern "C" {
 
 /* Armada 39x Family */
 #define MV_88F69XX		0x6900
+#define MV_6910_DEV_ID		0x6910
 #define MV_6920_DEV_ID		0x6920
+#define MV_6928_DEV_ID		0x6928
 
 /* BobCat2  Family */
 #define MV_BOBCAT2_DEV_ID		0xFC00
