5530e989e46ceaaca4b6304a81b5b22929a18145
Nadav Haklai <nadavh@marvell.com>
2014-12-10 13:29:33 +0200

    fix: mvebu: pm: a38x: Properly kill CPU1 when entering suspend to RAM
    	Align to the mainline HOTPLUG support for Armada 38x and
    	add missing cpu_die routine for Armada 38x
    
    	JIRA bug fix: #1355 - Kernel panic while enteting suspend to RAM
    
    Change-Id: Id9eca73d0629e158e5453cdbe14f88925978c505
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/15234
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/15299

--- arch/arm/mach-mvebu/pmsu.h
+++ arch/arm/mach-mvebu/pmsu.h
@@ -18,5 +18,5 @@ int mvebu_setup_boot_addr_wa(unsigned int crypto_eng_target,
 
 void mvebu_v7_pmsu_idle_exit(void);
 void armada_370_xp_cpu_resume(void);
-
+int armada_38x_do_cpu_suspend(unsigned long deepidle);
 #endif	/* __MACH_370_XP_PMSU_H */
