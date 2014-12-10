6ac11399c8bf39886b6b3178bcbeee08509dbae9
Grzegorz Jaszczyk <jaz@semihalf.com>
2014-12-10 15:24:04 +0100

    fix: thermal: add support for suspend/resume
    	- There is no need to implement subroutine for suspend since there is no
    	data to store before suspending
    	- Bug fix for: SYSTEMSW-1411 'Thermal sensor does not work after resume
    	from suspend'
    
    Change-Id: Ibf3df264f1abdef3fd21d2c300d08a70cdd54b1f
    Signed-off-by: Grzegorz Jaszczyk <jaz@semihalf.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/15257
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/15366
    Reviewed-on: http://vgitil04.il.marvell.com:8080/28001
    Reviewed-by: Ofer Heifetz <oferh@marvell.com>

--- drivers/thermal/armada_thermal.c
+++ drivers/thermal/armada_thermal.c
@@ -531,9 +531,23 @@ static int armada_thermal_exit(struct platform_device *pdev)
 	return 0;
 }
 
+static int armada_thermal_resume(struct platform_device *pdev)
+{
+	struct thermal_zone_device *thermal =
+		platform_get_drvdata(pdev);
+	struct armada_thermal_priv *priv = thermal->devdata;
+
+	priv->data->init_sensor(pdev, priv);
+
+	return 0;
+}
+
 static struct platform_driver armada_thermal_driver = {
 	.probe = armada_thermal_probe,
 	.remove = armada_thermal_exit,
+#ifdef CONFIG_PM
+	.resume = armada_thermal_resume,
+#endif
 	.driver = {
 		.name = "armada_thermal",
 		.of_match_table = armada_thermal_id_table,
