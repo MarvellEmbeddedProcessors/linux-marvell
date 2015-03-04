2cbba40f0134eba5d716c08689a59512a6903a5e
Nadav Haklai <nadavh@marvell.com>
2015-03-04 16:52:50 +0200

    a38x: Add initial support for Armada 383 and 384 flavors
    Change-Id: I23bd4eb772a9991d383b2a1d0b45121b6ff705dc
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/17244
    Tested-by: Star_Automation <star@marvell.com>

--- arch/arm/mach-mvebu/armada-38x.c
+++ arch/arm/mach-mvebu/armada-38x.c
@@ -169,6 +169,8 @@ static const char * const armada_380_dt_compat[] = {
 	"marvell,armada380",
 	"marvell,armada381",
 	"marvell,armada382",
+	"marvell,armada383",
+	"marvell,armada384",
 	"marvell,armada385",
 	"marvell,armada388",
 	NULL,
@@ -224,7 +226,7 @@ static void __init mvebu_memblock_reserve(void) {}
 
 
 
-DT_MACHINE_START(ARMADA_XP_DT, "Marvell Armada 380/381/382/385/388 (Device Tree)")
+DT_MACHINE_START(ARMADA_XP_DT, "Marvell Armada 380/381/382/383/384/385/388 (Device Tree)")
 	.smp		= smp_ops(armada_380_smp_ops),
 	.map_io		= debug_ll_io_init,
 	.init_irq	= irqchip_init,
