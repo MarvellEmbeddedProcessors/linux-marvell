0c2665c5c7a95dfaade6a26229c177eef74d172c
Nadav Haklai <nadavh@marvell.com>
2015-06-07 14:14:13 +0300

    fix: telephony: mvebu_phone: enable suspend/resume
    Enable proper preservation of all TDM registers.
    
    Fix for SYSTEMSW-1769 "No suspend/resume support in tdm interface in LKv3.10"
    
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    
    Change-Id: I3f5a0b87a0134a55df561a838509836f39a37386
    Reviewed-on: http://vgitil04.il.marvell.com:8080/19860
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>

--- drivers/telephony/mvebu_phone/mv_phone_dev.c
+++ drivers/telephony/mvebu_phone/mv_phone_dev.c
@@ -394,13 +394,26 @@ static int mvebu_phone_remove(struct platform_device *pdev)
 #ifdef CONFIG_PM_SLEEP
 static int mvebu_phone_suspend(struct device *dev)
 {
+	int i;
+
+	for (i = 0; i < TDM_CTRL_REGS_NUM; i++)
+		priv->tdm_ctrl_regs[i] = readl(priv->tdm_base + i);
+
+	for (i = 0; i < TDM_SPI_REGS_NUM; i++)
+		priv->tdm_spi_regs[i] = readl(priv->tdm_base +
+					      TDM_SPI_REGS_OFFSET + i);
+
+	priv->tdm_spi_mux_reg = MV_REG_READ(TDM_SPI_MUX_REG);
+	priv->tdm_mbus_config_reg = MV_REG_READ(TDM_MBUS_CONFIG_REG);
+	priv->tdm_misc_reg = MV_REG_READ(TDM_MISC_REG);
+
 	return 0;
 }
 
 static int mvebu_phone_resume(struct device *dev)
 {
 	struct platform_device *pdev = priv->parent;
-	int err;
+	int err, i;
 
 	err = mv_conf_mbus_windows(dev, priv->tdm_base,
 				   mv_mbus_dram_info());
@@ -414,7 +427,16 @@ static int mvebu_phone_resume(struct device *dev)
 			return err;
 	}
 
-	mvSysTdmInit(priv->tdm_params);
+	for (i = 0; i < TDM_CTRL_REGS_NUM; i++)
+		writel(priv->tdm_ctrl_regs[i], priv->tdm_base + i);
+
+	for (i = 0; i < TDM_SPI_REGS_NUM; i++)
+		writel(priv->tdm_spi_regs[i], priv->tdm_base +
+					      TDM_SPI_REGS_OFFSET + i);
+
+	MV_REG_WRITE(TDM_SPI_MUX_REG, priv->tdm_spi_mux_reg);
+	MV_REG_WRITE(TDM_MBUS_CONFIG_REG, priv->tdm_mbus_config_reg);
+	MV_REG_WRITE(TDM_MISC_REG, priv->tdm_misc_reg);
 
 	return 0;
 }
