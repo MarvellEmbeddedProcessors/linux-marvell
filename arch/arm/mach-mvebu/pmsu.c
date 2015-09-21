0b02525c6c91f3e42777bcd250e631391109a700
Nadav Haklai <nadavh@marvell.com>
2015-09-21 10:56:31 +0300

    ARM: mvebu: Disable CPU freq on Armada 38x for the multi CPU flavors
    On multi CPU Armada 38x SoCs, under heavy I/O load, the system hangs when
    cpufreq is enabled.
    Waiting for a solution to this issue, this patch disables the cpufreq
    support for those SoC.
    
    The single CPU flavor of Armada 38x, Armada 380 does not suffer from the
    cpufreq related system hang and therefore cpufreq can be enabled on this
    platform.
    
    Change-Id: Iae35574c2d538c01e22206724cf708691ba717b7
    Signed-off-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/23688
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/27314
    Tested-by: Lior Amsalem <alior@marvell.com>

--- arch/arm/mach-mvebu/pmsu.c
+++ arch/arm/mach-mvebu/pmsu.c
@@ -670,6 +670,12 @@ static int __init mvebu_pmsu_cpufreq_init(void)
 	if (!of_machine_is_compatible("marvell,armadaxp") &&
 	    !of_machine_is_compatible("marvell,armada380"))
 		return 0;
+
+	if (of_machine_is_compatible("marvell,armada380") &&
+	    (num_possible_cpus() > 1)) {
+		pr_warn("CPU freq is currently broken on Armada 38x: disabling");
+		return 0;
+	}
 	/*
 	 * In order to have proper cpufreq handling, we need to ensure
 	 * that the Device Tree description of the CPU clock includes
