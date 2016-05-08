c973362c76071ad0c9cdf807a1ec28dc74e3088d
Uri Eliyahu <uriel@marvell.com>
2016-05-08 15:02:15 +0300

    pp3: add pp3 to armada-390-db.dts
    Change-Id: Id302ddaa3e7e0dce6374e1102d0554e906f9950d
    Signed-off-by: Uri Eliyahu <uriel@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/29594
    Reviewed-by: Lior Amsalem <alior@marvell.com>
    Tested-by: Lior Amsalem <alior@marvell.com>

---
+++ include/dt-bindings/mvebu_nss/nss39x.h
@@ -0,0 +1,15 @@
+/*
+ * This header provides constants for NSS common values.
+ *
+ */
+
+#ifndef _DT_BINDINGS_MVEBU_NSS_H
+#define _DT_BINDINGS_MVEBU_NSS_H
+
+#define NSS_REGS_SIZE  0x1000000
+#define NSS_SPACE_SIZE 0x8000000
+#define NSS_TARGET_ID  0xC
+#define NSS_REGS_ATTR  0x4
+#define NSS_SPACE_ATTR 0x5
+
+#endif /* _DT_BINDINGS_MVEBU_NSS_H */
