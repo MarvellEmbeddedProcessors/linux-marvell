b040bd46927c25d415d34d2b084d278229ef8b62
Marcin Wojtas <mw@semihalf.com>
2016-06-02 13:24:24 +0200

    Revert "mm, page_alloc: remove MIGRATE_RESERVE"
    This is 3/3 of commits, whose purpose is to restore MIGRATE_RESERVE
    and mm state before v4.2 kernel. Its impact, according to Mel Gorman,
    was to give by coincidence, larger window for kswapd to do its work
    and prepare resources for intensive order 0 allocation. Originally
    MIGRATE_RESERVE was not designed for this purpose, but no issue
    was observed prior to its removal during the tests.
    
    This patch series disables improvements around page allocation
    merged into v4.4 kernel, which after tens of patches get efficient
    enough in v4.7-rc1 kernel, so that page alloc failures seem to
    eventually disappear.
    
    This reverts commit 974a786e63c9 ("mm, page_alloc: remove MIGRATE_RESERVE")
    
    Fix 3/4 of SYSTEMSW-2443: "Armada 3700 page alloc failures"
    
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    
    Conflicts:
    	mm/page_alloc.c
    	mm/vmstat.c
    
    Change-Id: I489e849512d298c0441234e303e93329a7d656bd
    Reviewed-on: http://vgitil04.il.marvell.com:8080/30481
    Reviewed-by: Lior Amsalem <alior@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -39,6 +39,8 @@ enum {
 	MIGRATE_UNMOVABLE,
 	MIGRATE_MOVABLE,
 	MIGRATE_RECLAIMABLE,
+	MIGRATE_PCPTYPES,	/* the number of types on the pcp lists */
+	MIGRATE_RESERVE = MIGRATE_PCPTYPES,
 #ifdef CONFIG_CMA
 	/*
 	 * MIGRATE_CMA migration type is designed to mimic the way
@@ -60,8 +62,6 @@ enum {
 #endif
 	MIGRATE_TYPES
 };
-
-#define MIGRATE_PCPTYPES (MIGRATE_RECLAIMABLE+1)
 
 #ifdef CONFIG_CMA
 #  define is_migrate_cma(migratetype) unlikely((migratetype) == MIGRATE_CMA)
@@ -429,6 +429,12 @@ struct zone {
 	unsigned long		present_pages;
 
 	const char		*name;
+
+	/*
+	 * Number of MIGRATE_RESERVE page block. To maintain for just
+	 * optimization. Protected by zone->lock.
+	 */
+	int			nr_migrate_reserve_block;
 
 #ifdef CONFIG_MEMORY_ISOLATION
 	/*
