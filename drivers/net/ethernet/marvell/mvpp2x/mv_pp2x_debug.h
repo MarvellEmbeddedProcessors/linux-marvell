3c692e68dba52f0a11da09b8071618d802763452
Alan Winkowski <walan@marvell.com>
2016-07-19 19:00:28 +0300

    mvpp2x: Use Linux macros for round_up/down, abs()
    Change-Id: I8b636f4b62c2e286b8ef6117b4c6442603c73bef
    Signed-off-by: Alan Winkowski <walan@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/31245
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- drivers/net/ethernet/marvell/mvpp2x/mv_pp2x_debug.h
+++ drivers/net/ethernet/marvell/mvpp2x/mv_pp2x_debug.h
@@ -24,33 +24,6 @@
 
 #define DBG_MSG(fmt, args...)	printk(fmt, ## args)
 
-/* Macro for alignment up. For example, MV_ALIGN_UP(0x0330, 0x20) = 0x0340   */
-#define MV_ALIGN_UP(number, align) (((number) & ((align) - 1)) ? \
-				    (((number) + (align)) & ~((align) - 1)) : \
-				    (number))
-
-/* Macro for alignment down. For example, MV_ALIGN_UP(0x0330, 0x20) = 0x0320 */
-#define MV_ALIGN_DOWN(number, align) ((number) & ~((align) - 1))
-
-/* CPU architecture dependent 32, 16, 8 bit read/write IO addresses */
-#define MV_MEMIO32_WRITE(addr, data)    \
-	((*((unsigned int *)(addr))) = ((unsigned int)(data)))
-
-#define MV_MEMIO32_READ(addr)           \
-	((*((unsigned int *)(addr))))
-
-#define MV_MEMIO16_WRITE(addr, data)    \
-	((*((unsigned short *)(addr))) = ((unsigned short)(data)))
-
-#define MV_MEMIO16_READ(addr)           \
-	((*((unsigned short *)(addr))))
-
-#define MV_MEMIO8_WRITE(addr, data)     \
-	((*((unsigned char *)(addr))) = ((unsigned char)(data)))
-
-#define MV_MEMIO8_READ(addr)            \
-	((*((unsigned char *)(addr))))
-
 void mv_pp2x_skb_dump(struct sk_buff *skb, int size, int access);
 
 int mv_pp2x_debug_param_set(u32 param);
