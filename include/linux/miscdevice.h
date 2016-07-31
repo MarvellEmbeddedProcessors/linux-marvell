57a6ac831ad97945d0a533ddc2276c9b23f10735
Igal Liberman <igall@marvell.com>
2016-07-31 14:59:34 +0300

    miscdevice: mvebu: add devices for marvell telephony drivers
    Add 2 miscdevices for Marvell telephony drivers:
    - SLIC device - might have different names for different manufacturers
    - TAL device - user-space interface for telephony tool
    
    Change-Id: Ib7e2df46196d4a0b6a5b7dcd5d26e7eaa21c99c2
    Signed-off-by: Igal Liberman <igall@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/31564
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>

--- include/linux/miscdevice.h
+++ include/linux/miscdevice.h
@@ -19,6 +19,8 @@
 #define APOLLO_MOUSE_MINOR	7	/* unused */
 #define PC110PAD_MINOR		9	/* unused */
 /*#define ADB_MOUSE_MINOR	10	FIXME OBSOLETE */
+#define SLICDEV_MINOR           73      /* Marvell SLIC control device */
+#define TALDEV_MINOR            74      /* Marvell TAL device */
 #define WATCHDOG_MINOR		130	/* Watchdog timer     */
 #define TEMP_MINOR		131	/* Temperature Sensor */
 #define RTC_MINOR		135
