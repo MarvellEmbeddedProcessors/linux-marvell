79696cc7b1aed480e0eb93d22ccc55b008729579
Omri Itach <omrii@marvell.com>
2016-12-27 18:11:33 +0200

    misc: mvebu: add soc info driver for package revision
    Some drivers require differentiation between SoC revision A0,A1,B0, etc.
    SATA AHCI driver reads SoC revision directly from AHCI mvebu wrapper.
    
    Other drivers also require this information to differentiate SoC revisions.
    In order to avoid code duplication across drivers, add a dedicated driver
    to read the SoC revision information.
    
    Change-Id: I4bf53abf5bfa80332172e0937a67aefb7a72d471
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Signed-off-by: Konstantin Porotchkin <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/55606
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-by: Victor Gu <xigu@marvell.com>

---
+++ include/linux/mv_soc_info.h
@@ -0,0 +1,23 @@
+/*
+ * Marvell A8K SoC info definitions.
+ *
+ * Copyright (C) 2018 Marvell Semiconductor
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef MV_SOC_INFO_H
+#define MV_SOC_INFO_H
+
+#define GWD_IIDR2_REV_ID_OFFSET	12
+#define GWD_IIDR2_REV_ID_MASK	0xF
+#define APN806_REV_ID_A0	0
+#define APN806_REV_ID_A1	1
+#define APN806_REV_ID_B0	2
+
+int mv_soc_info_get_ap_revision(void);
+
+
+#endif /* MV_SOC_INFO_H */
