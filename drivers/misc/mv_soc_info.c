8cc9a3e384ebd83b032b08e863501d543c0e0451
Omri Itach <omrii@marvell.com>
2016-12-27 18:11:33 +0200

    ap806: add soc info driver for package revision
    Several drivers require differentiation between SoC revision A0 and A1.
    SATA aHCI driver reads SoC revision directly from aHCI mvebu wrapper.
    
    Other drivers also require this information to differentiate SoC revision,
    So to avoid this code duplication across drivers, added a dedicated driver
    to read SoC revision information.
    
    Change-Id: I4bf53abf5bfa80332172e0937a67aefb7a72d471
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/35109
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

---
+++ drivers/misc/mv_soc_info.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2015-2016 Marvell International Ltd.
+
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <linux/clk.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_irq.h>
+#include <linux/platform_device.h>
+#include <linux/mv_soc_info.h>
+#include <linux/of_address.h>
+
+int soc_revision_id = -1;
+
+/* Get SoC revision ID */
+int mv_soc_info_get_revision(void)
+{
+	void __iomem *ap806_rev_info_reg;
+	const unsigned int *reg;
+	struct device_node *node;
+	phys_addr_t paddr;
+
+	/* if soc_revision_id was already initialized, no need to read it again */
+	if (soc_revision_id != -1)
+		return soc_revision_id;
+
+	node = of_find_compatible_node(NULL, NULL, "marvell,ap806-rev-info");
+	if (!node) {
+		pr_err("Read soc-rev-info node failed\n");
+		goto free_node;
+	}
+
+	/* Read the offset of the register */
+	reg = of_get_property(node, "reg", NULL);
+	if (!reg) {
+		pr_err("Read reg property failed from soc-rev-info node\n");
+		goto free_node;
+	}
+
+	/* Translate the offset to physical address */
+	paddr = of_translate_address(node, reg);
+	if (paddr == OF_BAD_ADDR) {
+		pr_err("of_translate_address failed for soc-rev-info\n");
+		goto free_node;
+	}
+
+	ap806_rev_info_reg = ioremap(paddr, reg[1]);
+	if (!ap806_rev_info_reg) {
+		pr_err("ioremap() failed for soc-rev-info register\n");
+		goto free_node;
+	}
+
+	/* read gwd_iidr2 register and set the revision global*/
+	soc_revision_id = (readl(ap806_rev_info_reg) >> GWD_IIDR2_REV_ID_OFFSET) &
+			   GWD_IIDR2_REV_ID_MASK;
+
+	pr_debug("%s: Detected SoC revision A%d\n",
+		 __func__, soc_revision_id == APN806_REV_ID_A0 ? 0 : 1);
+
+	/* Release resources */
+	iounmap(ap806_rev_info_reg);
+	of_node_put(node);
+
+	return soc_revision_id;
+
+free_node:
+	of_node_put(node);
+	return -EINVAL;
+}
