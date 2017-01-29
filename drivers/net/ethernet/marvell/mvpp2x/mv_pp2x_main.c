263befee174a48f58db87fd47df52db5a0eeaef5
Stefan Chulski <stefanc@marvell.com>
2017-01-29 17:47:44 +0200

    fix: net: mvpp2x: fix wrong RXQ recorded in skb->queue_mapping issue
    Issue:
    - Physical RXQ ID were recorded in skb->queue_mapping instead of
      logical CoS RXQ ID.
    - Recording wrong RXQ ID cause crashes and warnings in GRO
      receive path.
    
    Fix:
    - Record logical CoS RXQ ID.
    
    Change-Id: I44d591155c50a5fc96cc79171ac7aa5ff2a87ce5
    Signed-off-by: Stefan Chulski <stefanc@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/36234
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>

--- drivers/net/ethernet/marvell/mvpp2x/mv_pp2x_main.c
+++ drivers/net/ethernet/marvell/mvpp2x/mv_pp2x_main.c
@@ -2420,7 +2420,7 @@ err_drop_frame:
 		skb_put(skb, rx_bytes);
 		skb->protocol = eth_type_trans(skb, dev);
 		mv_pp2x_rx_csum(port, rx_status, skb);
-		skb_record_rx_queue(skb, (u16)rxq->id);
+		skb_record_rx_queue(skb, (u16)rxq->log_id);
 		mv_pp2x_set_skb_hash(rx_desc, rx_status, skb);
 		skb_mark_napi_id(skb, napi);
 
