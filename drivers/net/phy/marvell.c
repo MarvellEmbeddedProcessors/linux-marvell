f7245a7425931ec565527aa18192151315ceb351
Stefan Chulski <stefanc@marvell.com>
2017-03-20 13:36:59 +0200

    phy: marvell: Add Marvell copyright
    Change-Id: I8e569be64893a518ea98dd41bc3316cce3efdc6b
    Signed-off-by: Stefan Chulski <stefanc@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/37656
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>

--- drivers/net/phy/marvell.c
+++ drivers/net/phy/marvell.c
@@ -9,6 +9,8 @@
  *
  * Copyright (c) 2013 Michael Stapelberg <michael@stapelberg.de>
  *
+ * Copyright (C) 2016 Marvell International Ltd.
+ *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
  * Free Software Foundation;  either version 2 of the  License, or (at your
