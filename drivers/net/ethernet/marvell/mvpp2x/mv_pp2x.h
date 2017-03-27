8ed8fb59a015ebe76a66265bbe5770bea87c9749
Stefan Chulski <stefanc@marvell.com>
2017-03-27 13:23:50 +0300

    net: mvpp2x: Add mvpp2x driver CPU hot-plug support
    This patch add mvpp2x driver CPU hotplug support.
    Only MVPP22 support CPU hotplug.
    
    Driver register two CPU hotplug notifiers:
    1. CP level notifier - reconfigure RSS, if driver in multi queue mode.
    2. Port level notifier - set irq affinity for private interrupts,
       if port UP.
    
    Instead of using for_each_online_cpu that run only on online CPU,
    for_each_present_cpu would be used and MVPP22 register would be
    accessed from relevant CPU address space.
    Nothing changed in MVPP21 functionality.
    
    Also added preempt_disable to prevent CPU migration (due to hot plug)
    during per cpu pointers access.
    
    Change-Id: Ia288a019bde130769ed7f80adcafb64744d77360
    Signed-off-by: Stefan Chulski <stefanc@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/38021
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- drivers/net/ethernet/marvell/mvpp2x/mv_pp2x.h
+++ drivers/net/ethernet/marvell/mvpp2x/mv_pp2x.h
@@ -545,6 +545,7 @@ struct mv_pp2x {
 
 	struct delayed_work stats_task;
 	struct workqueue_struct *workqueue;
+	struct notifier_block	cp_hotplug_nb;
 };
 
 struct mv_pp2x_pcpu_stats {
@@ -643,6 +644,7 @@ struct mv_pp2x_port {
 	struct mv_pp2x_ptp_desc *ptp_desc;
 	struct mv_pp2x_cos cos_cfg;
 	struct mv_pp2x_rss rss_cfg;
+	struct notifier_block	port_hotplug_nb;
 };
 
 struct pp2x_hw_params {
