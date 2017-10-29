6c54db0665cba1f9507d08633a10a569723a4fcf
Ofer Heifetz <oferh@marvell.com>
2017-10-29 11:00:28 +0200

    crypto: inside-secure: remove unused invalidate param
    safexcel_context is not used in safexcel_invalidate_cache
    so it is removed.
    
    Change-Id: Iffc8118c41f0dcd6532e5ce1fc05f4660fa8ffd0
    Signed-off-by: Ofer Heifetz <oferh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/45625
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Igal Liberman <igall@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- drivers/crypto/inside-secure/safexcel.c
+++ drivers/crypto/inside-secure/safexcel.c
@@ -833,7 +833,6 @@ void safexcel_inv_complete(struct crypto_async_request *req, int error)
 
 /* Context cache invalidation */
 int safexcel_invalidate_cache(struct crypto_async_request *async,
-			      struct safexcel_context *ctx,
 			      struct safexcel_crypto_priv *priv,
 			      dma_addr_t ctxr_dma,
 			      int ring, struct safexcel_request *request)
