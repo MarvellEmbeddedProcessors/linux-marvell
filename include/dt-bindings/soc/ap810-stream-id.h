1cfbd27e5a6315ccd0a32525d829f77b9ebfbf75
Omri Itach <omrii@marvell.com>
2017-11-30 15:34:19 +0200

    dts: a8kp: add device trees for Armada-8082 & Armada-8084 DB boards
    device trees include preliminary support for board interfaces:
    
    Armada-8082 - 8 cores with 2 CPs:
    CP0 SATA1, PCIe0x4, 2*USB2 ports
    CP1 SATA1, PCIe0x4
    
    Armada8084 - 8 cores with 4 CPs:
    CP0 SATA1, PCIe0x4, 2*USB2 ports
    CP1 SATA1, PCIe0x4
    CP2 SATA1
    CP3 SATA1
    
    Notes:
    	- CP XOR engines disabled due to missing MSI interrupts stream ID
    	  configuration.
    	- AXI monitors disabled due to missing system controller (clock) driver.
    
    Change-Id: I3995c08473367b7973a0f5b0b78d8bcd02e84b24
    Signed-off-by: Omri Itach <omrii@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/47102
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- include/dt-bindings/soc/ap810-stream-id.h
+++ include/dt-bindings/soc/ap810-stream-id.h
@@ -1,3 +1,19 @@
-/* XOR stream IDs: 0xa0-0xaf */
+/*
+ * All the Stream id calculation based on ATF configuration.
+ * Reference for this calculation can be found under
+ * <atf root dir>/drivers/marvell/mochi/<required setup>.h
+ */
+
+/* AP XOR stream IDs: 0xa0-0xaf */
 #define MVEBU_STREAM_ID_XOR0		0xa0
 #define MVEBU_STREAM_ID_XORx(x, ap)	(MVEBU_STREAM_ID_XOR0 + ap * 4 + x)
+
+/* CP(n) XOR0 stream ID: 0x89 + 11*n */
+#define MVEBU_STREAM_ID_CP0_XOR0	0x89
+/*
+ * There are 12 required stream ids per CP,
+ * but only 11 of them are unique (two SATA ports get the same stream id).
+ */
+#define STREAM_PER_CP_COUNT		11
+#define MVEBU_STREAM_ID_CPx_XORx(xor_num, cp110_num) \
+		(MVEBU_STREAM_ID_CP0_XOR0 + (cp110_num * STREAM_PER_CP_COUNT) + xor_num)
