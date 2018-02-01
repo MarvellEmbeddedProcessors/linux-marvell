494b83d028ce7f150ca6131e785a19611bb5177f
Marc Zyngier <marc.zyngier@arm.com>
2018-02-01 11:07:35 +0000

    ARM: KVM: invalidate BTB on guest exit for Cortex-A12/A17
    In order to avoid aliasing attacks against the branch predictor,
    let's invalidate the BTB on guest exit. This is made complicated
    by the fact that we cannot take a branch before invalidating the
    BTB.
    
    We only apply this to A12 and A17, which are the only two ARM
    cores on which this useful.
    
    Change-Id: I9413130abf25d443f6b0e194c116f31697eaa62b
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Boot-tested-by: Tony Lindgren <tony@atomide.com>
    Reviewed-by: Tony Lindgren <tony@atomide.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/58011
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- arch/arm/include/asm/kvm_asm.h
+++ arch/arm/include/asm/kvm_asm.h
@@ -89,8 +89,6 @@ extern char __kvm_hyp_init_end[];
 extern char __kvm_hyp_exit[];
 extern char __kvm_hyp_exit_end[];
 
-extern char __kvm_hyp_vector[];
-
 extern char __kvm_hyp_code_start[];
 extern char __kvm_hyp_code_end[];
 
--- arch/arm/include/asm/kvm_mmu.h
+++ arch/arm/include/asm/kvm_mmu.h
@@ -274,7 +274,24 @@ static inline void __kvm_extend_hypmap(pgd_t *boot_hyp_pgd,
 
 static inline void *kvm_get_hyp_vector(void)
 {
-	return __kvm_hyp_vector;
+	switch (read_cpuid_part()) {
+#ifdef CONFIG_HARDEN_BRANCH_PREDICTOR
+	case ARM_CPU_PART_CORTEX_A12:
+	case ARM_CPU_PART_CORTEX_A17:
+	{
+		extern char __kvm_hyp_vector_bp_inv[];
+
+		return kvm_ksym_ref(__kvm_hyp_vector_bp_inv);
+	}
+
+#endif
+	default:
+	{
+		extern char __kvm_hyp_vector[];
+
+		return kvm_ksym_ref(__kvm_hyp_vector);
+	}
+	}
 }
 
 static inline int kvm_map_vectors(void)
