8e4b09ab1e1672d268677ba553a4ead54e8c8a9e
David Sniatkiwicz <davidsn@marvell.com>
2018-02-15 11:12:24 +0200

    dts: a8kp: xor: align xor engine stream-ids to atf
    align xor engines stream-ids to atf
    	- set AP810 xor stream-id base to 0x100 instead of 0xA0
    	- set all the CPs stream-id base to 0x80
    	- set all the CPs xor stream-id base to 0x89
    
    Change-Id: Ibfcfb95460ea046dd1fa79f4a32061c6655512d0
    Signed-off-by: David Sniatkiwicz <davidsn@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/52609
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- include/dt-bindings/soc/ap810-stream-id.h
+++ include/dt-bindings/soc/ap810-stream-id.h
@@ -4,16 +4,20 @@
  * <atf root dir>/drivers/marvell/mochi/<required setup>.h
  */
 
-/* AP XOR stream IDs: 0xa0-0xaf */
-#define MVEBU_STREAM_ID_AP0_XOR0	0xa0
-#define MVEBU_STREAM_ID_APx_XORx(x, ap)	(MVEBU_STREAM_ID_AP0_XOR0 + ap * 4 + x)
+/* AP XOR stream IDs: 0x100-0xFFF */
+#define MVEBU_STREAM_ID_AP0_XOR0		0x100
+/* max stream IDs per AP */
+#define MVEBU_STREAM_ID_AP_MAX			0x200
+#define MVEBU_STREAM_ID_APx_XORx(x, ap)		(MVEBU_STREAM_ID_AP0_XOR0 + \
+						(ap * MVEBU_STREAM_ID_AP_MAX) + x)
 
-/* CP(n) XOR0 stream ID: 0x89 + 11*n */
-#define MVEBU_STREAM_ID_CP0_XOR0	0x89
-/*
- * There are 12 required stream ids per CP,
- * but only 11 of them are unique (two SATA ports get the same stream id).
- */
-#define STREAM_PER_CP_COUNT		11
-#define MVEBU_STREAM_ID_CPx_XORx(xor_num, cp110_num) \
-		(MVEBU_STREAM_ID_CP0_XOR0 + (cp110_num * STREAM_PER_CP_COUNT) + xor_num)
+/* CP stream ID base */
+#define MVEBU_STREAM_ID_BASE			0x80
+
+/* CP XOR0 stream ID offset */
+#define MVEBU_STREAM_ID_CP_XOR0_OFFSET		0x9
+
+/* CP XOR0 stream ID: 0x89 */
+#define MVEBU_STREAM_ID_CP_XOR0			(MVEBU_STREAM_ID_BASE + MVEBU_STREAM_ID_CP_XOR0_OFFSET)
+
+#define MVEBU_STREAM_ID_CP_XORx(xor_num)	(MVEBU_STREAM_ID_CP_XOR0 + xor_num)
