454428c2f7878a92f419391d9459d99b62d1a03a
Yan Markman <ymarkman@marvell.com>
2018-02-24 08:51:08 +0200

    net: mvpp2: fix: dma mask size 40 bits
    Problem:
    DMA-API debugger reports about unpaired dma-map/unmap,
    where the unmap is executed on addresses 0x1xxxxxxxxxx,
    e.g. address size here is 41 bits instead of 40 really supported.
    
    FIX:
    Set coorect GENMASK_ULL(39, 0) bit-size 40 from bit 0 till bit 39.
    
    Change-Id: Iba2065b1cf43b7135ed8cd1c1b0fa51ca2db6c04
    Signed-off-by: Yan Markman <ymarkman@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/50970
    Reviewed-on: http://vgitil04.il.marvell.com:8080/52780
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Victor Gu <xigu@marvell.com>

--- drivers/net/ethernet/marvell/mvpp2.c
+++ drivers/net/ethernet/marvell/mvpp2.c
@@ -1440,7 +1440,7 @@ static dma_addr_t mvpp2_txdesc_dma_addr_get(struct mvpp2_port *port,
 	if (port->priv->hw_version == MVPP21)
 		return tx_desc->pp21.buf_dma_addr;
 	else
-		return tx_desc->pp22.buf_dma_addr_ptp & GENMASK_ULL(40, 0);
+		return tx_desc->pp22.buf_dma_addr_ptp & GENMASK_ULL(39, 0);
 }
 
 static void mvpp2_txdesc_dma_addr_set(struct mvpp2_port *port,
@@ -1458,7 +1458,7 @@ static void mvpp2_txdesc_dma_addr_set(struct mvpp2_port *port,
 	} else {
 		u64 val = (u64)addr;
 
-		tx_desc->pp22.buf_dma_addr_ptp &= ~GENMASK_ULL(40, 0);
+		tx_desc->pp22.buf_dma_addr_ptp &= ~GENMASK_ULL(39, 0);
 		tx_desc->pp22.buf_dma_addr_ptp |= val;
 		tx_desc->pp22.packet_offset = offset;
 	}
@@ -1518,7 +1518,7 @@ static dma_addr_t mvpp2_rxdesc_dma_addr_get(struct mvpp2_port *port,
 	if (port->priv->hw_version == MVPP21)
 		return rx_desc->pp21.buf_dma_addr;
 	else
-		return rx_desc->pp22.buf_dma_addr_key_hash & GENMASK_ULL(40, 0);
+		return rx_desc->pp22.buf_dma_addr_key_hash & GENMASK_ULL(39, 0);
 }
 
 static unsigned long mvpp2_rxdesc_cookie_get(struct mvpp2_port *port,
@@ -1527,7 +1527,7 @@ static unsigned long mvpp2_rxdesc_cookie_get(struct mvpp2_port *port,
 	if (port->priv->hw_version == MVPP21)
 		return rx_desc->pp21.buf_cookie;
 	else
-		return rx_desc->pp22.buf_cookie_misc & GENMASK_ULL(40, 0);
+		return rx_desc->pp22.buf_cookie_misc & GENMASK_ULL(39, 0);
 }
 
 static size_t mvpp2_rxdesc_size_get(struct mvpp2_port *port,
