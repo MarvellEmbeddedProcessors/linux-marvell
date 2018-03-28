90e6943821f928f023d38a40dfc67353f7de3f09
Florian Fainelli <f.fainelli@gmail.com>
2018-03-28 15:44:15 -0700

    net: phy: phylink: Provide PHY interface to mac_link_{up, down}
    In preparation for having DSA transition entirely to PHYLINK, we need to pass a
    PHY interface type to the mac_link_{up,down} callbacks because we may have to
    make decisions on that (e.g: turn on/off RGMII interfaces etc.). We do not pass
    an entire phylink_link_state because not all parameters (pause, duplex etc.) are
    defined when the link is down, only link and interface are.
    
    Update mvneta accordingly since it currently implements phylink_mac_ops.
    
    Change-Id: Ibca7683630a8b619785ea94b3a413098c2f1f999
    Acked-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/54534
    Reviewed-by: Victor Gu <xigu@marvell.com>
    Tested-by: Victor Gu <xigu@marvell.com>

--- drivers/net/ethernet/marvell/mvneta.c
+++ drivers/net/ethernet/marvell/mvneta.c
@@ -3440,7 +3440,8 @@ static void mvneta_set_eee(struct mvneta_port *pp, bool enable)
 	mvreg_write(pp, MVNETA_LPI_CTRL_1, lpi_ctl1);
 }
 
-static void mvneta_mac_link_down(struct net_device *ndev, unsigned int mode)
+static void mvneta_mac_link_down(struct net_device *ndev, unsigned int mode,
+				 phy_interface_t interface)
 {
 	struct mvneta_port *pp = netdev_priv(ndev);
 	u32 val;
@@ -3459,6 +3460,7 @@ static void mvneta_mac_link_down(struct net_device *ndev, unsigned int mode)
 }
 
 static void mvneta_mac_link_up(struct net_device *ndev, unsigned int mode,
+			       phy_interface_t interface,
 			       struct phy_device *phy)
 {
 	struct mvneta_port *pp = netdev_priv(ndev);
