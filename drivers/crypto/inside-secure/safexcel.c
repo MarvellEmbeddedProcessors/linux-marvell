57c420c346d6c13250d1cd8be05e4cca3dd846a9
Ofer Heifetz <oferh@marvell.com>
2018-04-11 09:20:29 +0300

    crypto: inside-secure - authenc(hmac(sha512),cbc(aes)) support
    Change-Id: I3530e51ee5db5188cb5c45badfa9b7d53b78a273
    Signed-off-by: Antoine Tenart <antoine.tenart@bootlin.com>
    Signed-off-by: Ofer Heifetz <oferh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/55728

--- drivers/crypto/inside-secure/safexcel.c
+++ drivers/crypto/inside-secure/safexcel.c
@@ -1080,6 +1080,7 @@ static struct safexcel_alg_template *safexcel_algs[] = {
 	&safexcel_alg_authenc_hmac_sha1_cbc_aes,
 	&safexcel_alg_authenc_hmac_sha224_cbc_aes,
 	&safexcel_alg_authenc_hmac_sha256_cbc_aes,
+	&safexcel_alg_authenc_hmac_sha512_cbc_aes,
 };
 
 /* Register the supported hash and cipher algorithms */
