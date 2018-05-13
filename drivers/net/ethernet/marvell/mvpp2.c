fb926e35476b129988d5334599d16646a8c8c6ef
Yuval Caduri <cyuval@marvell.com>
2018-05-13 09:14:46 +0300

    fix: pp2: Disable egress handling for musdk port
    Egress handling should not be performed for MUSDK ports,
    it is the responsibility of musdk itself.
    In the existing code, mvpp2_egress_disable() disables
    tx again and again with delays.
    
    Change-Id: Ic6f755271982f723afc21a4fe6b7bcbc6bc1aa72
    Signed-off-by: Yuval Caduri <cyuval@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/54923
    Reviewed-by: Stefan Chulski <stefanc@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Victor Gu <xigu@marvell.com>

--- drivers/net/ethernet/marvell/mvpp2.c
+++ drivers/net/ethernet/marvell/mvpp2.c
@@ -8243,6 +8243,9 @@ static void mvpp2_egress_enable(struct mvpp2_port *port)
 	int queue;
 	int tx_port_num = mvpp2_egress_port(port);
 
+	if (port->flags && MVPP22_F_IF_MUSDK)
+		return;
+
 	/* Enable all initialized TXs. */
 	qmap = 0;
 	for (queue = 0; queue < port->ntxqs; queue++) {
@@ -8264,6 +8267,9 @@ static void mvpp2_egress_disable(struct mvpp2_port *port)
 	u32 reg_data;
 	int delay;
 	int tx_port_num = mvpp2_egress_port(port);
+
+	if (port->flags && MVPP22_F_IF_MUSDK)
+		return;
 
 	/* Issue stop command for active channels only */
 	mvpp2_write(port->priv, MVPP2_TXP_SCHED_PORT_INDEX_REG, tx_port_num);
