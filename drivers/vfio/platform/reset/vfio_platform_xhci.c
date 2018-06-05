ae31b2b9f6051d0795320ca0865356162224216e
David Sniatkiwicz <davidsn@marvell.com>
2018-06-05 17:27:22 +0300

    vfio: xhci: update support for XHCI reset hook
    The VFIO reset hook is called every time a platform device is
    passed to a guest or removed from a guest.
    When the XHCI device is unbound from the host, the host driver
    disables the XHCI phys so when the device is passed to the
    guest it becomes dis-functional.
    This patch uses the VFIO reset hook to enable
    the XHCI phys on behalf of the guest.
    
    Change-Id: I59de5de33bc59542f70db6bdc2f88728cfe8df0c
    Signed-off-by: David Sniatkiwicz <davidsn@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/56407
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- drivers/vfio/platform/reset/vfio_platform_xhci.c
+++ drivers/vfio/platform/reset/vfio_platform_xhci.c
@@ -24,6 +24,8 @@
 #include <linux/device.h>
 #include <linux/of.h>
 #include <linux/clk.h>
+#include <linux/phy/phy.h>
+#include <linux/usb/phy.h>
 
 #include "vfio_platform_private.h"
 
@@ -32,11 +34,16 @@
 #define DRIVER_DESC     "Reset support for XHCI vfio platform device"
 
 #define MAX_XHCI_CLOCKS		4
+#define MAX_XHCI_PHYS		2
 
 int vfio_platform_xhci_reset(struct vfio_platform_device *vdev)
 {
-	struct device_node *np = vdev->device->of_node;
+	struct device *dev = vdev->device;
+	struct device_node *np = dev->of_node;
 	struct clk *clk;
+	struct phy *phy;
+	struct usb_phy *usb_phy;
+	struct regulator *current_limiter_regulator;
 	int ret, i;
 
 	for (i = 0; i < MAX_XHCI_CLOCKS; i++) {
@@ -46,6 +53,31 @@ int vfio_platform_xhci_reset(struct vfio_platform_device *vdev)
 			if (ret)
 				return -ENODEV;
 		}
+	}
+
+	for (i = 0; i < MAX_XHCI_PHYS; i++) {
+		phy = devm_of_phy_get_by_index(dev, np, i);
+		if (!IS_ERR(phy)) {
+			ret = phy_power_on(phy);
+			if (ret)
+				return -ENODEV;
+		}
+	}
+
+	usb_phy = devm_usb_get_phy_by_phandle(dev, "usb-phy", 0);
+	if (!IS_ERR(usb_phy)) {
+		ret = usb_phy_init(usb_phy);
+		if (ret)
+			return -ENODEV;
+		current_limiter_regulator =
+			devm_regulator_get_optional(usb_phy->dev,
+						    "current-limiter");
+		if (!IS_ERR(current_limiter_regulator)) {
+			if (regulator_enable(current_limiter_regulator))
+				dev_err(dev,
+					"Failed to enable Current-limiter regulator\n");
+		}
+
 	}
 
 	return 0;
