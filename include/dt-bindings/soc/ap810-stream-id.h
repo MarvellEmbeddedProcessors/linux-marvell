4669a92d6eb70bd607e9e04feb6aae8ecee6a121
David Sniatkiwicz <davidsn@marvell.com>
2018-06-11 17:17:10 +0300

    arm64: a8kp: align stream-ids to firmware
    This patch aligns all the SIDs to the firmware.
    
    Change-Id: I1388aa14aa6022a769b1570c777f5c45f62a6ceb
    Signed-off-by: David Sniatkiwicz <davidsn@marvell.com>

--- include/dt-bindings/soc/ap810-stream-id.h
+++ include/dt-bindings/soc/ap810-stream-id.h
@@ -12,12 +12,20 @@
 						(ap * MVEBU_STREAM_ID_AP_MAX) + x)
 
 /* CP stream ID base */
-#define MVEBU_STREAM_ID_BASE			0x80
+#define MVEBU_STREAM_ID_BASE			0x40
+
+/* CP max stream IDs */
+#define MVEBU_MAX_STREAM_ID_PER_CP		0x10
 
 /* CP XOR0 stream ID offset */
-#define MVEBU_STREAM_ID_CP_XOR0_OFFSET		0x9
+#define MVEBU_STREAM_ID_CP_XOR0_OFFSET		0x2
 
-/* CP XOR0 stream ID: 0x89 */
-#define MVEBU_STREAM_ID_CP_XOR0			(MVEBU_STREAM_ID_BASE + MVEBU_STREAM_ID_CP_XOR0_OFFSET)
+/* CP XOR0 stream ID: 0x42 */
+#define MVEBU_STREAM_ID_CP0_XOR0		(MVEBU_STREAM_ID_BASE + \
+						MVEBU_STREAM_ID_CP_XOR0_OFFSET)
 
-#define MVEBU_STREAM_ID_CP_XORx(xor_num)	(MVEBU_STREAM_ID_CP_XOR0 + xor_num)
+#define MVEBU_STREAM_ID_CPx_XORx(cp_num, xor_num)	\
+	(MVEBU_STREAM_ID_CP0_XOR0 + \
+	((cp_num  % 4) * MVEBU_MAX_STREAM_ID_PER_CP) + xor_num)
+
+
