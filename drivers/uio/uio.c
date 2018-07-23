b99df1e125a80182fe75aa0aa5335f48f323e6d2
Dmitri Epshtein <dima@marvell.com>
2018-07-23 14:04:32 +0300

    Revert "fix: uio: block thread calling uio_poll() by updating event counter"
    commit e3853b942b0e
    ("fix: uio: block thread calling uio_poll() by updating event counter")
    
    Change-Id: I41a2994ee4073ffad8f2c3671d8697ad95a7531c
    Signed-off-by: Dmitri Epshtein <dima@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/58443
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Omri Itach <omrii@marvell.com>

--- drivers/uio/uio.c
+++ drivers/uio/uio.c
@@ -492,17 +492,13 @@ static unsigned int uio_poll(struct file *filep, poll_table *wait)
 {
 	struct uio_listener *listener = filep->private_data;
 	struct uio_device *idev = listener->dev;
-	s32 event_count;
 
 	if (!idev->info->irq)
 		return -EIO;
 
 	poll_wait(filep, &idev->wait, wait);
-	event_count = atomic_read(&idev->event);
-	if (listener->event_count != event_count) {
-		listener->event_count = event_count;
+	if (listener->event_count != atomic_read(&idev->event))
 		return POLLIN | POLLRDNORM;
-	}
 	return 0;
 }
 
