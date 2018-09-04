4725809ea447239c2be1bdd6c740d5f0a547700f
Yan Markman <ymarkman@marvell.com>
2018-09-04 17:35:55 +0300

    net: mvpp2: sync rx buffer before unmap
    This patch fixes the "RCU stall on cpu".
    The stall always occurred by TX in iperf-TCP(TSO)-bidirectional
    scenario. The stall is inspired by bad TCP RX ack buffer.
    Finding:
    The RX buffer _unmap_ is asynchroneous and further data processing
    may be incorrect.
    FIX:
    Make _sync_ operation before _unmap_ to guaranty the data coherency.
    
    Change-Id: I75d754780ca5b35eabf4138bbed506120b67a304
    Signed-off-by: Yan Markman <ymarkman@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/59725
    Reviewed-by: Stefan Chulski <stefanc@marvell.com>
    Tested-by: Stefan Chulski <stefanc@marvell.com>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
@@ -3207,6 +3207,10 @@ err_drop_frame:
 		else
 			frag_size = bm_pool->frag_size;
 
+		/* _sync_ for coherency (_unmap_ is asynchroneous) */
+		dma_sync_single_for_cpu(dev->dev.parent, dma_addr,
+					MVPP2_RX_BUF_SIZE(rx_bytes),
+					DMA_FROM_DEVICE);
 		prefetch(data + NET_SKB_PAD); /* packet header */
 
 		dma_unmap_single(dev->dev.parent, dma_addr,
