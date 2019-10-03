25f569a1e8606855cd0fec8390d80e95a32d9630
Sujeet Baranwal <sbaranwal@marvell.com>
2019-10-03 13:01:48 -0700

    scmi: perf: octtx2: round off OPP frequencies to neaest 100
    Calculated frequencies based on the perf value and multiplier leads
    to values which are not integral multiple of 100. octx2 chipsets
    needs the frequencies to be integral multiple of 100.
    
    Signed-off-by: Sujeet Baranwal <sbaranwal@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/16841
    
    Signed-off-by: Wojciech Bartczak <wbartczak@marvell.com>
    Change-Id: I8fd14bd88a6c2d83d9d2f6a4c5fd7613baf4078d
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/48455
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/115476
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/firmware/arm_scmi/perf.c
+++ drivers/firmware/arm_scmi/perf.c
@@ -873,6 +873,23 @@ static void scmi_perf_domain_init_fc(const struct scmi_protocol_handle *ph,
 	dom->fc_info = fc;
 }
 
+static uint32_t roundoff_to_nearest_100(uint32_t freq)
+{
+	uint32_t quotient, remainder;
+	uint32_t freq_mhz;
+
+	freq_mhz = (freq / 1000000);
+	quotient = freq_mhz / 100;
+	remainder = freq_mhz % 100;
+
+	if (remainder >= 50)
+		freq_mhz = quotient * 100 + 100;
+	else
+		freq_mhz = quotient * 100;
+
+	return freq_mhz * 1000000;
+}
+
 static int scmi_dvfs_device_opps_add(const struct scmi_protocol_handle *ph,
 				     struct device *dev, u32 domain)
 {
@@ -897,6 +914,11 @@ static int scmi_dvfs_device_opps_add(const struct scmi_protocol_handle *ph,
 		data.level = dom->opp[idx].perf;
 		data.freq = freq;
 
+		/*
+		 * marvell specific: need to round off to nearest hundered
+		 * if the calcuated frequency is not a multiple of 100 in MHz
+		 */
+		data.freq = roundoff_to_nearest_100(freq);
 		ret = dev_pm_opp_add_dynamic(dev, &data);
 		if (ret) {
 			dev_warn(dev, "[%d][%s]: Failed to add OPP[%d] %lu\n",
@@ -984,7 +1006,8 @@ static int scmi_dvfs_freq_get(const struct scmi_protocol_handle *ph, u32 domain,
 		return ret;
 
 	if (!dom->level_indexing_mode) {
-		*freq = level * dom->mult_factor;
+		/* Note domain is validated implicitly by scmi_perf_level_get */
+		*freq = roundoff_to_nearest_100(level * dom->mult_factor);
 	} else {
 		struct scmi_opp *opp;
 
