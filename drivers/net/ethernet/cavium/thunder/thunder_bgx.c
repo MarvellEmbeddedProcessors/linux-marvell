951c787e7592161c60abecdebc181f4ab8020628
Sunil Goutham <sgoutham@marvell.com>
2020-04-06 12:47:20 +0530

    net: thunderx: Do a PCS reset upon SGMII link toggle
    When physical link toggles with BGX mode being SGMII, BGX to Serdes link
    needs to be reset for it function normally otherwise packet IO won't work.
    
    This patch does the PCS reset to get IO functioning again When the traffic
    is running, if we unplug the Ethernet cable from SGMII port and plug it
    again, it become malfunction.
    
    Change-Id: Ic5eaddd3b583f43e9568a6da532e638f4eb61116
    Signed-off-by: Prakash Brahmajyosyula <bprakash@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/10407
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/cavium/thunder/thunder_bgx.c
+++ drivers/net/ethernet/cavium/thunder/thunder_bgx.c
@@ -80,6 +80,7 @@ static struct bgx *bgx_vnic[MAX_BGX_THUNDER];
 static int lmac_count; /* Total no of LMACs in system */
 
 static int bgx_xaui_check_link(struct lmac *lmac);
+static int bgx_lmac_sgmii_init(struct bgx *bgx, struct lmac *lmac);
 
 /* Supported devices */
 static const struct pci_device_id bgx_id_table[] = {
@@ -579,6 +580,13 @@ static void bgx_sgmii_change_link_state(struct lmac *lmac)
 	}
 	bgx_reg_write(bgx, lmac->lmacid, BGX_GMP_PCS_MISCX_CTL, misc_ctl);
 	bgx_reg_write(bgx, lmac->lmacid, BGX_GMP_GMI_PRTX_CFG, port_cfg);
+
+	bgx_reg_modify(bgx, lmac->lmacid, BGX_GMP_PCS_MRX_CTL,
+		       PCS_MRX_CTL_RESET);
+	if (bgx_poll_reg(bgx, lmac->lmacid, BGX_GMP_PCS_MRX_CTL,
+			 PCS_MRX_CTL_RESET, true)) {
+		dev_err(&bgx->pdev->dev, "BGX PCS reset not completed\n");
+	}
 
 	/* Restore CMR config settings */
 	cmr_cfg |= (rx_en ? CMR_PKT_RX_EN : 0) | (tx_en ? CMR_PKT_TX_EN : 0);
