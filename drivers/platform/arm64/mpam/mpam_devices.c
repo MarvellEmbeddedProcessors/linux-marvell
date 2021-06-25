ae8ed2c5c8d25c2dcb5cd442dca783dc896c7ada
James Morse <james.morse@arm.com>
2021-06-25 12:53:12 +0100

    arm_mpam: Add helpers to allocate monitors
    MPAM's MSC support a number of monitors, each of which supports
    bandwidth counters, or cache-storage-utilisation counters. To use
    a counter, a monitor needs to be configured. Add helpers to allocate
    and free CSU or MBWU monitors.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/platform/arm64/mpam/mpam_devices.c
+++ drivers/platform/arm64/mpam/mpam_devices.c
@@ -331,6 +331,8 @@ mpam_class_alloc(u8 level_idx, enum mpam_class_types type, gfp_t gfp)
 	class->level = level_idx;
 	class->type = type;
 	INIT_LIST_HEAD_RCU(&class->classes_list);
+	ida_init(&class->ida_csu_mon);
+	ida_init(&class->ida_mbwu_mon);
 
 	list_add_rcu(&class->classes_list, &mpam_classes);
 
