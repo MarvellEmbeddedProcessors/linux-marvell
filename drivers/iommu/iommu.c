03bb2b17994f709f13c437e4891d4cc4a24f3a95
James Morse <james.morse@arm.com>
2021-09-16 16:19:43 +0100

    iommu: Add helpers to retrieve iommu_groups by id or kobject
    ARM SMMU with MPAM support are able to mark streams of traffic with
    the QoS labels MPAM uses. The user-space interface for MPAM is the
    resctrl filesystem, which allows threads to be moved between groups,
    its natural to do the same for iommu_groups.
    
    The resctrl interface lists threads, so will also need to list
    iommu_groups, it will be necessary to walk the list of iommu_groups.
    To ensure this matches what user-space sees via sysfs, it is best
    to walk the kobjects.
    
    When making a change, resctrl will only have the id of a group.
    To avoid walking the list of kobjects in this case, add
    iommu_group_get_by_id().
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Change-Id: I58e8d9620ef72f2f57a1ad87728adcaf48ba8ec2

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -982,6 +982,40 @@ struct iommu_group *iommu_group_alloc(void)
 }
 EXPORT_SYMBOL_GPL(iommu_group_alloc);
 
+struct iommu_group *iommu_group_get_from_kobj(struct kobject *group_kobj)
+{
+	struct iommu_group *group;
+
+	if (!iommu_group_kset || !group_kobj)
+		return NULL;
+
+	group = container_of(group_kobj, struct iommu_group, kobj);
+
+	kobject_get(group->devices_kobj);
+	kobject_put(&group->kobj);
+
+	return group;
+}
+
+struct iommu_group *iommu_group_get_by_id(int id)
+{
+	struct kobject *group_kobj;
+	const char *name;
+
+	if (!iommu_group_kset)
+		return NULL;
+
+	name = kasprintf(GFP_KERNEL, "%d", id);
+	if (!name)
+		return NULL;
+
+	group_kobj = kset_find_obj(iommu_group_kset, name);
+	kfree(name);
+
+	return iommu_group_get_from_kobj(group_kobj);
+}
+EXPORT_SYMBOL_GPL(iommu_group_get_by_id);
+
 /**
  * iommu_group_get_iommudata - retrieve iommu_data registered for a group
  * @group: the group
