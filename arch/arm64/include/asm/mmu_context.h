43b13b4e073971e073c876077f34db55a0e58dba
Alex Belits <abelits@marvell.com>
2022-11-09 15:33:43 +0530

    arm64: Add support for ASID locking
    This patch adds support for ASID locking to be used with firmware
    assisted interrupts.
    
    Change-Id: I82a99848bdecb8a7a6f62b4e0cb8fd3b070e2a3e
    Signed-off-by: Alex Belits <abelits@marvell.com>
    Signed-off-by: Sujeet Baranwal <sbaranwal@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/8805
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Sujeet Kumar Baranwal <Sujeet.Baranwal@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/26936
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>

--- arch/arm64/include/asm/mmu_context.h
+++ arch/arm64/include/asm/mmu_context.h
@@ -313,6 +313,12 @@ static inline bool arch_vma_access_permitted(struct vm_area_struct *vma,
 
 #include <asm-generic/mmu_context.h>
 
+#ifdef CONFIG_MRVL_OCTEONTX_EL0_INTR
+int lock_context(struct mm_struct *mm, int index);
+int unlock_context_by_index(int index);
+bool unlock_context_by_mm(struct mm_struct *mm);
+#endif
+
 #endif /* !__ASSEMBLY__ */
 
 #endif /* !__ASM_MMU_CONTEXT_H */
