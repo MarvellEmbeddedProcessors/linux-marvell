f5ba99bb77eefa6a12300a3bea1fa8ffd1242d70
Hariprasad Kelam <hkelam@marvell.com>
2022-11-20 22:07:59 +0530

    octeontx2-af: Add programmed macaddr to RVU pfvf
    This commit adds the mac address that is programmed from
    application into the RVU pfvf structure. The mac address
    retrieval will return the address from RVU pfvf structure.
    
    Change-Id: I1a4a91fdff91c53629a08ffc9e1557588ce775a0
    Signed-off-by: Vidhya Vidhyaraman <Vidhya.Raman@cavium.com>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cgx.c
@@ -759,6 +759,7 @@ int rvu_mbox_handler_cgx_mac_addr_set(struct rvu *rvu,
 				      struct cgx_mac_addr_set_or_get *rsp)
 {
 	int pf = rvu_get_pf(req->hdr.pcifunc);
+	struct rvu_pfvf *pfvf;
 	u8 cgx_id, lmac_id;
 
 	if (!is_pf_cgxmapped(rvu, pf))
@@ -770,6 +771,8 @@ int rvu_mbox_handler_cgx_mac_addr_set(struct rvu *rvu,
 
 	rvu_get_cgx_lmac_id(rvu->pf2cgxlmac_map[pf], &cgx_id, &lmac_id);
 
+	pfvf = &rvu->pf[pf];
+	memcpy(pfvf->mac_addr, req->mac_addr, ETH_ALEN);
 	cgx_lmac_addr_set(cgx_id, lmac_id, req->mac_addr);
 
 	return 0;
@@ -847,20 +850,12 @@ int rvu_mbox_handler_cgx_mac_addr_get(struct rvu *rvu,
 				      struct cgx_mac_addr_set_or_get *req,
 				      struct cgx_mac_addr_set_or_get *rsp)
 {
-	int pf = rvu_get_pf(req->hdr.pcifunc);
-	u8 cgx_id, lmac_id;
-	int rc = 0;
-	u64 cfg;
+	struct rvu_pfvf *pfvf = rvu_get_pfvf(rvu, req->hdr.pcifunc);
 
-	if (!is_cgx_config_permitted(rvu, req->hdr.pcifunc))
-		return -EPERM;
+	if (!is_pf_cgxmapped(rvu, rvu_get_pf(req->hdr.pcifunc)))
+		return LMAC_AF_ERR_PF_NOT_MAPPED;
 
-	rvu_get_cgx_lmac_id(rvu->pf2cgxlmac_map[pf], &cgx_id, &lmac_id);
-
-	rsp->hdr.rc = rc;
-	cfg = cgx_lmac_addr_get(cgx_id, lmac_id);
-	/* copy 48 bit mac address to req->mac_addr */
-	u64_to_ether_addr(cfg, rsp->mac_addr);
+	memcpy(rsp->mac_addr, pfvf->mac_addr, ETH_ALEN);
 	return 0;
 }
 
