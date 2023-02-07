a39be545dfd7923d5176acb39de01fc6b04d409a
Rohit Mathew <rohit.mathew@arm.com>
2023-02-07 19:14:17 +0000

    arm_mpam: Probe for long/lwd mbwu counters
    mpam v0.1 and versions above v1.0 support optional long counter for
    memory bandwidth monitoring. The MPAMF_MBWUMON_IDR register have fields
    indicating support for long counters. As of now, a 44 bit counter
    represented by HAS_LONG field (bit 30) and a 63 bit counter represented
    by LWD (bit 29) can be optionally integrated. Probe for these counters
    and set corresponding feature bits if any of these counters are present.
    
    Signed-off-by: Rohit Mathew <rohit.mathew@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/platform/arm64/mpam/mpam_devices.c
+++ drivers/platform/arm64/mpam/mpam_devices.c
@@ -837,7 +837,7 @@ static void mpam_ris_hw_probe(struct mpam_msc_ris *ris)
 			}
 		}
 		if (FIELD_GET(MPAMF_MSMON_IDR_MSMON_MBWU, msmon_features)) {
-			bool hw_managed;
+			bool has_long, hw_managed;
 			u32 mbwumonidr = mpam_read_partsel_reg(msc, MBWUMON_IDR);
 
 			props->num_mbwu_mon = FIELD_GET(MPAMF_MBWUMON_IDR_NUM_MON, mbwumonidr);
@@ -847,11 +847,31 @@ static void mpam_ris_hw_probe(struct mpam_msc_ris *ris)
 			if (FIELD_GET(MPAMF_MBWUMON_IDR_HAS_RWBW, mbwumonidr))
 				mpam_set_feature(mpam_feat_msmon_mbwu_rwbw, props);
 
+			/*
+			 * Treat long counter and its extension, lwd as mutually
+			 * exclusive feature bits. Though these are dependent
+			 * fields at the implementation level, there would never
+			 * be a need for mpam_feat_msmon_mbwu_44counter (long
+			 * counter) and mpam_feat_msmon_mbwu_63counter (lwd)
+			 * bits to be set together.
+			 *
+			 * mpam_feat_msmon_mbwu isn't treated as an exclusive
+			 * bit as this feature bit would be used as the "front
+			 * facing feature bit" for any checks related to mbwu
+			 * monitors.
+			 */
+			has_long = FIELD_GET(MPAMF_MBWUMON_IDR_HAS_LONG, mbwumonidr);
+			if (props->num_mbwu_mon && has_long) {
+				if (FIELD_GET(MPAMF_MBWUMON_IDR_LWD, mbwumonidr))
+					mpam_set_feature(mpam_feat_msmon_mbwu_63counter, props);
+				else
+					mpam_set_feature(mpam_feat_msmon_mbwu_44counter, props);
+			}
+
 			/* Is NRDY hardware managed? */
 			mpam_ris_hw_probe_hw_nrdy(ris, MBWU, hw_managed);
 			if (hw_managed)
 				mpam_set_feature(mpam_feat_msmon_mbwu_hw_nrdy, props);
-
 		}
 	}
 
--- drivers/platform/arm64/mpam/mpam_internal.h
+++ drivers/platform/arm64/mpam/mpam_internal.h
@@ -180,7 +180,15 @@ enum mpam_device_features {
 	mpam_feat_msmon_csu,
 	mpam_feat_msmon_csu_capture,
 	mpam_feat_msmon_csu_hw_nrdy,
+
+	/*
+	 * Having mpam_feat_msmon_mbwu set doesn't mean the regular 31 bit MBWU
+	 * counter would be used. The exact counter used is decided based on the
+	 * status of mpam_feat_msmon_mbwu_l/mpam_feat_msmon_mbwu_lwd as well.
+	 */
 	mpam_feat_msmon_mbwu,
+	mpam_feat_msmon_mbwu_44counter,
+	mpam_feat_msmon_mbwu_63counter,
 	mpam_feat_msmon_mbwu_capture,
 	mpam_feat_msmon_mbwu_rwbw,
 	mpam_feat_msmon_mbwu_hw_nrdy,
