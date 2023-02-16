a73176bb53ed54dd36f7037c1c98a0b3847a39e2
Suman Ghosh <sumang@marvell.com>
2023-02-16 15:00:13 +0530

    rcu: Print cpu state during a stall
    During RCU stall, print additional information about CPU
    state using the newly introduced print_arch_cpu_state() API.
    
    On ARM64 architecture this API prints the CPU's PC value,
    Secure/Non-Secure Execution state and the Exception level
    corresponding to the PC value.
    
    Change-Id: I3a753ff1c593b0f93bb8311731d6d8ddf09ad0e8
    Signed-off-by: Suman Ghosh <sumang@marvell.com>
    Signed-off-by: Tanmay Jagdale <tanmay@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/102768
    Tested-by: Linu Cherian <lcherian@marvell.com>
    Reviewed-by: Linu Cherian <lcherian@marvell.com>

--- kernel/rcu/tree_stall.h
+++ kernel/rcu/tree_stall.h
@@ -16,6 +16,11 @@
 //
 // Controlling CPU stall warnings, including delay calculation.
 
+void __weak print_arch_cpu_state(int cpu)
+{
+	/* Nothing to do */
+}
+
 /* panic() on RCU Stall sysctl. */
 int sysctl_panic_on_rcu_stall __read_mostly;
 int sysctl_max_rcu_stall_to_panic __read_mostly;
@@ -527,6 +532,7 @@ static void print_cpu_stall_info(int cpu)
 	       falsepositive ? " (false positive?)" : "");
 
 	print_cpu_stat_info(cpu);
+	print_arch_cpu_state(cpu);
 }
 
 /* Complain about starvation of grace-period kthread.  */
