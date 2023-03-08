f3ac06c11319d7cfe35c9d7c696766e827f26ae1
George Cherian <george.cherian@marvell.com>
2023-03-08 11:55:54 +0530

    spi: spi-nor: Fix the spi_nor_panic_write
    Ported from 5.4
    
    spi_nor_panic_write is not working properly as necessary steps are not
    done to avoid sleeping and lock implications.
    
    Add equivalent non sleeping and lockless calls for
    1. Call nor->prepare and nor->unprepare across the writes
    2. Call write_enable() before writing
    3. Check for spi_nor_ready in between writes
    
    Fixes: f7bebda6e882 ("drivers: mtd: spi-nor: Support kmsg dumper based
    on pstore/blk")
    Signed-off-by: George Cherian <george.cherian@marvell.com>
    Reviewed-by: Bhaskara Budiredla <bbudiredla@marvell.com>
    Reviewed-by: Witold Sadowski <wsadowski@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    
    Change-Id: I32c412de5bc8af56d2b38aa3707dd145419e663f
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -2140,6 +2140,14 @@ static int spi_nor_panic_write(struct mtd_info *mtd, loff_t to, size_t len,
 	size_t page_offset, page_remain, i;
 	ssize_t ret;
 
+	if (nor->controller_ops &&  nor->controller_ops->prepare) {
+		ret = nor->controller_ops->prepare(nor);
+		if (ret) {
+			dev_err(nor->dev, "failed in the preparation.\n");
+			return ret;
+		}
+	}
+
 	nor->pstore = 1;
 	for (i = 0; i < len; ) {
 		ssize_t written;
@@ -2151,15 +2159,25 @@ static int spi_nor_panic_write(struct mtd_info *mtd, loff_t to, size_t len,
 		page_remain = min_t(size_t,
 				    nor->info->page_size - page_offset, len - i);
 
+		ret = spi_nor_write_enable(nor);
+		if (ret)
+			return ret;
+
 		ret = spi_nor_write_data(nor, addr, page_remain, buf + i);
 		if (ret < 0)
 			return ret;
 
 		written = ret;
 
+		while (!spi_nor_ready(nor))
+			;
+
 		*retlen += written;
 		i += written;
 	}
+
+	if (nor->controller_ops && nor->controller_ops->unprepare)
+		nor->controller_ops->unprepare(nor);
 
 	return 0;
 }
