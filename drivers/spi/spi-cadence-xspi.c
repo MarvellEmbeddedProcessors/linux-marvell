9841172137a1ece0a1a3f5e84bfd892a4b13a659
Witold Sadowski <wsadowski@marvell.com>
2023-05-16 09:53:53 -0700

    cn20kx: spi: Reconfigure xSPI config to STIG Mode
    This change is required to reconfigure the xSPI to STIG Mode
    before starting the transfer message in case the mode has been
    changed by ATF xSPI in response to an SMC call.
    
    Signed-off-by: Witold Sadowski <wsadowski@marvell.com>
    Signed-off-by: Piyush Malgujar <pmalgujar@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/103564
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/115981
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Change-Id: I4e0814a1096809b6b7a1389194e9f973d4018f78

--- drivers/spi/spi-cadence-xspi.c
+++ drivers/spi/spi-cadence-xspi.c
@@ -1090,6 +1090,12 @@ static int cdns_xspi_transfer_one_message_b0(struct spi_controller *controller,
 	int cs = spi_get_chipselect(spi, 0);
 	int cs_change = 0;
 
+	if (cdns_xspi_wait_for_controller_idle(cdns_xspi) < 0)
+		return -EIO;
+
+	writel(FIELD_PREP(CDNS_XSPI_CTRL_WORK_MODE, CDNS_XSPI_WORK_MODE_STIG),
+	       cdns_xspi->iobase + CDNS_XSPI_CTRL_CONFIG_REG);
+
 	/* Enable xfer state machine */
 	if (!cdns_xspi->xfer_in_progress) {
 		u32 xfer_control = readl(cdns_xspi->xferbase + MRVL_XFER_FUNC_CTRL);
