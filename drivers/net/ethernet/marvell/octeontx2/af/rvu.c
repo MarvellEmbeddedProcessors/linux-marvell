068a8adc5155721bb52db508f5ec9533e08d6845
Pavan Nikhilesh <pbhagavatula@marvell.com>
2023-11-07 19:17:01 +0530

    octeontx2-af: add TIM error af interrupt handlers
    Add interrupt handlers for TIM_AF_BKT_SKIP and TIM_AF_RVU_INT
    interrupts.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: Ifcd46c3104354fd57ffb2e15b9df0eca4e730ae9
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/116003
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -3273,6 +3273,7 @@ static void rvu_unregister_interrupts(struct rvu *rvu)
 	int irq;
 
 	rvu_sso_unregister_interrupts(rvu);
+	rvu_tim_unregister_interrupts(rvu);
 	rvu_cpt_unregister_interrupts(rvu);
 
 	/* Disable the Mbox interrupt */
@@ -3486,6 +3487,10 @@ static int rvu_register_interrupts(struct rvu *rvu)
 	rvu->irq_allocated[offset] = true;
 
 	ret = rvu_sso_register_interrupts(rvu);
+	if (ret)
+		goto fail;
+
+	ret = rvu_tim_register_interrupts(rvu);
 	if (ret)
 		goto fail;
 
