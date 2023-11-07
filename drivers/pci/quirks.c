58fad8c98afe9ac977a10320bd06363c5f462ae7
Wojciech Bartczak <wbartczak@marvell.com>
2023-11-07 04:11:39 -0800

    PCI: Fix XCP0 BAR0 address reported by config space on cnf10kb
    Cnf10kb reports wrong address range for XCP0 BAR0.
    This causes error while loading Marvell Message-Handling-Unit
    device driver.
    Following patch add necessary modification to PCI quirks.
    
    Signed-off-by: Wojciech Bartczak <wbartczak@marvell.com>
    Change-Id: Ia5eafee78851c2dc00b50cdf57b8aa9058a0940b
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/76787
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/96256
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/115481
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/148457

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -6469,12 +6469,14 @@ DECLARE_PCI_FIXUP_FINAL(0x5555, 0x0004, pci_fixup_d3cold_delay_1sec);
 	(((addr) & CAVIUM_XCP0_FIX_MASK) != CAVIUM_XCP0_ADDR_OK)
 #define CAVIUM_XCP0_FIX_ADDR(addr) \
 	(((addr) & (~CAVIUM_XCP0_FIX_MASK)) | CAVIUM_XCP0_ADDR_OK)
+#define CAVIUM_XCP0_FIX_SOC(subsys) \
+	(((subsys) == 0xba00) || ((subsys) == 0xbc00))
 
 static void quirk_cavium_xcp0_bar_fixup(struct pci_dev *dev)
 {
 	int i;
 
-	if (dev->subsystem_device == 0xba00 && dev->devfn == 0xe0) {
+	if (CAVIUM_XCP0_FIX_SOC(dev->subsystem_device) && dev->devfn == 0xe0) {
 		for (i = 0; i < PCI_STD_RESOURCE_END; i++) {
 
 			struct resource *r = &dev->resource[i];
