3b3f76142dad2e866a468e8a02d03e8ca9d2a8c0
Wojciech Bartczak <wbartczak@marvell.com>
2023-11-07 04:08:46 -0800

    PCI: Fix wrong address reported for XCP0 mailbox on cnf10ka
    Following change adds a quirk to fix wrong address reported
    for BAR 0, XCP0 for cnf10ka SoC. XCP0 is used by mvl_mhu mailbox driver.
    Without the fix communication through SCMI protocol is impossible.
    
    Signed-off-by: Wojciech Bartczak <wbartczak@marvell.com>
    Change-Id: Idf96a2f40002d7d22827e6a2a98eaed8a9f2b7ee
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/67651
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/96255
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/115480
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/148456

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -6456,3 +6456,42 @@ static void pci_fixup_d3cold_delay_1sec(struct pci_dev *pdev)
 	pdev->d3cold_delay = 1000;
 }
 DECLARE_PCI_FIXUP_FINAL(0x5555, 0x0004, pci_fixup_d3cold_delay_1sec);
+
+/* Marvell cnf10ka (0xba00) requires fix for device at slot (0xe), func. 0x0
+ * Wrong values for BAR0 and BAR4 are fetched from config space.
+ * There are some devices that doesn't require fixing, so the fix is not always
+ * applied. Deciding factor is curent value of BAR0/BAR4.
+ * Config. space for cnf10ka is read-only, Changing the registers isn't possible
+ */
+#define CAVIUM_XCP0_ADDR_OK	0x000082c000000000ULL /* Correct PCI BAR base */
+#define CAVIUM_XCP0_FIX_MASK	0xffffffff00000000ULL
+#define CAVIUM_XCP0_SHOULD_FIX(addr) \
+	(((addr) & CAVIUM_XCP0_FIX_MASK) != CAVIUM_XCP0_ADDR_OK)
+#define CAVIUM_XCP0_FIX_ADDR(addr) \
+	(((addr) & (~CAVIUM_XCP0_FIX_MASK)) | CAVIUM_XCP0_ADDR_OK)
+
+static void quirk_cavium_xcp0_bar_fixup(struct pci_dev *dev)
+{
+	int i;
+
+	if (dev->subsystem_device == 0xba00 && dev->devfn == 0xe0) {
+		for (i = 0; i < PCI_STD_RESOURCE_END; i++) {
+
+			struct resource *r = &dev->resource[i];
+			int ret;
+
+			if (!(r->flags & IORESOURCE_MEM))
+				continue;
+
+			/* There are revision of HW that not need fixup */
+			if (CAVIUM_XCP0_SHOULD_FIX(r->start)) {
+				r->start = CAVIUM_XCP0_FIX_ADDR(r->start);
+				r->end = CAVIUM_XCP0_FIX_ADDR(r->end);
+				ret = pci_claim_resource(dev, i);
+				pci_info(dev, "Fixup (%d) %llx - %llx/%lx. (%d)\n",
+					 i, r->start, r->end, r->flags, ret);
+			}
+		}
+	}
+}
+DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_CAVIUM, 0xa067, quirk_cavium_xcp0_bar_fixup);
