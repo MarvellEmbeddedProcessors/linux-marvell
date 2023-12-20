42d7de0da2ca485b6d5bf2723430d5bf2759f72d
Sai Krishna <saikrishnag@marvell.com>
2023-12-21 00:08:01 +0530

    octeontx2-pf: Add ucast filter count configurability via devlink.
    Added a devlink param to set/modify unicast filter count. Currently
    it's hardcoded with a macro.
    
    Signed-off-by: Sai Krishna <saikrishnag@marvell.com>
    Change-Id: Ibceb4c9993fc5f626fcee0a86f759e890baa591f
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/118873
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_devlink.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_devlink.c
@@ -261,12 +261,75 @@ static int otx2_dl_serdes_link_get(struct devlink *devlink, u32 id,
 	return 0;
 }
 
+static int otx2_dl_ucast_flt_cnt_set(struct devlink *devlink, u32 id,
+				     struct devlink_param_gset_ctx *ctx)
+{
+	struct otx2_devlink *otx2_dl = devlink_priv(devlink);
+	struct otx2_nic *pfvf = otx2_dl->pfvf;
+	int err;
+
+	pfvf->flow_cfg->ucast_flt_cnt = ctx->val.vu8;
+
+	otx2_mcam_flow_del(pfvf);
+	err = otx2_mcam_entry_init(pfvf);
+	if (err)
+		return err;
+
+	return 0;
+}
+
+static int otx2_dl_ucast_flt_cnt_get(struct devlink *devlink, u32 id,
+				     struct devlink_param_gset_ctx *ctx)
+{
+	struct otx2_devlink *otx2_dl = devlink_priv(devlink);
+	struct otx2_nic *pfvf = otx2_dl->pfvf;
+
+	if (!pfvf->flow_cfg) {
+		ctx->val.vu8 = 0;
+		return 0;
+	}
+
+	ctx->val.vu8 = pfvf->flow_cfg->ucast_flt_cnt;
+
+	return 0;
+}
+
+static int otx2_dl_ucast_flt_cnt_validate(struct devlink *devlink, u32 id,
+					  union devlink_param_value val,
+					  struct netlink_ext_ack *extack)
+{
+	struct otx2_devlink *otx2_dl = devlink_priv(devlink);
+	struct otx2_nic *pfvf = otx2_dl->pfvf;
+
+	/* Check for UNICAST filter support*/
+	if (!(pfvf->flags & OTX2_FLAG_UCAST_FLTR_SUPPORT)) {
+		NL_SET_ERR_MSG_MOD(extack,
+				   "Unicast filter not enabled");
+		return -EINVAL;
+	}
+
+	if (!pfvf->flow_cfg) {
+		NL_SET_ERR_MSG_MOD(extack,
+				   "pfvf->flow_cfg not initialized");
+		return -EINVAL;
+	}
+
+	if (pfvf->flow_cfg->nr_flows) {
+		NL_SET_ERR_MSG_MOD(extack,
+				   "Cannot modify count when there are active rules");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 enum otx2_dl_param_id {
 	OTX2_DEVLINK_PARAM_ID_BASE = DEVLINK_PARAM_GENERIC_ID_MAX,
 	OTX2_DEVLINK_PARAM_ID_MCAM_COUNT,
 	OTX2_DEVLINK_PARAM_ID_TL1_RR_PRIO,
 	OTX2_DEVLINK_PARAM_ID_CQE_SIZE,
 	OTX2_DEVLINK_PARAM_ID_RBUF_SIZE,
+	OTX2_DEVLINK_PARAM_ID_UCAST_FLT_CNT,
 	OTX2_DEVLINK_PARAM_ID_SERDES_LINK,
 };
 
@@ -296,6 +359,11 @@ static const struct devlink_param otx2_dl_params[] = {
 			     BIT(DEVLINK_PARAM_CMODE_RUNTIME),
 			     otx2_dl_serdes_link_get, otx2_dl_serdes_link_set,
 			     NULL),
+	DEVLINK_PARAM_DRIVER(OTX2_DEVLINK_PARAM_ID_UCAST_FLT_CNT,
+			     "unicast_filter_count", DEVLINK_PARAM_TYPE_U8,
+			     BIT(DEVLINK_PARAM_CMODE_RUNTIME),
+			     otx2_dl_ucast_flt_cnt_get, otx2_dl_ucast_flt_cnt_set,
+			     otx2_dl_ucast_flt_cnt_validate),
 };
 
 #ifdef CONFIG_RVU_ESWITCH
