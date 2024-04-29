6413efc3ecd4a0700eb6398e23ca1422593ebb12
Geetha sowjanya <gakula@marvell.com>
2024-04-29 10:51:52 +0530

    Marvell: cn20k: Add cn20k silicon check
    This patch implements cn20k silicon check using
    [0:7] bit of subsystem id. The subsystem id is
    programmed as 0x20 for all devices on cn20k
    silicons.
    
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Change-Id: I354576f50c092d3c3ce064ff0fefefec40fa8272
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/126281
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/126276
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

---
+++ include/linux/soc/marvell/silicons.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ * Copyright (C) 2024 Marvell.
+ */
+
+#ifndef __SOC_SILICON_H
+#define __SOC_SILICON_H
+
+#include <linux/types.h>
+#include <linux/pci.h>
+
+#if defined(CONFIG_ARM64)
+
+#define CN20K_CHIPID	0x20
+/*
+ * Silicon check for CN20K family
+ */
+static inline bool is_cn20k(struct pci_dev *pdev)
+{
+	if ((pdev->subsystem_device & 0xFF) == CN20K_CHIPID)
+		return true;
+
+	return false;
+}
+#else
+#define is_cn20k(pdev)          ({ 0; })
+#endif
+
+#endif /* __SOC_SILICON_H */
