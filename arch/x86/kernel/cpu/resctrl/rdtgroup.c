1a65b919b2273747a143f2f75af1490ec59bd194
James Morse <james.morse@arm.com>
2024-06-03 18:23:17 +0100

    x86/resctrl: Use schema type to determine the schema format string
    Resctrl's architecture code gets to specify a format string that is
    used when printing schema entries. This is expected to be one of two
    values that the filesystem code supports.
    
    Setting this format string allows the architecture code to change
    the ABI resctrl presents to user-space.
    
    Instead, use the schema format enum to choose which format string to
    use.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    ---
    Change since v4:
     * Added a stop to a struct comment.
    
    Changes since v2:
     * This patch is new.

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -2600,6 +2600,15 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	if (cl > max_name_width)
 		max_name_width = cl;
 
+	switch (r->schema_fmt) {
+	case RESCTRL_SCHEMA_BITMAP:
+		s->fmt_str = "%d=%0*x";
+		break;
+	case RESCTRL_SCHEMA_RANGE:
+		s->fmt_str = "%d=%0*u";
+		break;
+	}
+
 	INIT_LIST_HEAD(&s->list);
 	list_add(&s->list, &resctrl_schema_all);
 
