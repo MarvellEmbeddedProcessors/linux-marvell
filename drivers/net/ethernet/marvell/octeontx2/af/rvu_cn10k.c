c2b80b9d669e81422c00254b9448ebed26535fb3
Subbaraya Sundeep <sbhatta@marvell.com>
2024-06-05 20:51:37 +0530

    octeontx2-af: Enable LMT_ENA in APR_LMT_MAP_ENTRY_S
    Enable the LMT line of a PF/VF by enabling LMT_ENA
    bit in the structure APR_LMT_MAP_ENTRY_S.
    Also simplify logic of calculating LMTST table index by using
    max number of PFs i.e, 256 always.
    
    Change-Id: I173f6c11003a03027368fbfe39b34de46b0d9ebd
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Sai Krishna <saikrishnag@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/133397
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
@@ -14,6 +14,10 @@
 #define LMT_TBL_OP_READ		0
 #define LMT_TBL_OP_WRITE	1
 #define LMT_MAPTBL_ENTRY_SIZE	16
+#define LMT_MAX_PF_INDEX	256
+
+#define LMT_MAP_ENTRY_ENA      BIT_ULL(20)
+#define LMT_MAP_ENTRY_LINES    GENMASK_ULL(18, 16)
 
 /* Function to perform operations (read/write) on lmtst map table */
 static int lmtst_map_table_ops(struct rvu *rvu, u32 index, u64 *val,
@@ -39,6 +43,13 @@ static int lmtst_map_table_ops(struct rvu *rvu, u32 index, u64 *val,
 		*val = readq(lmt_map_base + index);
 	} else {
 		writeq((*val), (lmt_map_base + index));
+
+		cfg = FIELD_PREP(LMT_MAP_ENTRY_ENA, 0x1);
+		/* 2048 LMTLINES */
+		cfg |= FIELD_PREP(LMT_MAP_ENTRY_LINES, 0x6);
+
+		writeq(cfg, (lmt_map_base + (index + 8)));
+
 		/* Flushing the AP interceptor cache to make APR_LMT_MAP_ENTRY_S
 		 * changes effective. Write 1 for flush and read is being used as a
 		 * barrier and sets up a data dependency. Write to 0 after a write
@@ -56,7 +67,7 @@ static int lmtst_map_table_ops(struct rvu *rvu, u32 index, u64 *val,
 #define LMT_MAP_TBL_W1_OFF  8
 static u32 rvu_get_lmtst_tbl_index(struct rvu *rvu, u16 pcifunc)
 {
-	return ((rvu_get_pf(pcifunc) * rvu->hw->total_vfs) +
+	return ((rvu_get_pf(pcifunc) * LMT_MAX_PF_INDEX) +
 		(pcifunc & RVU_PFVF_FUNC_MASK)) * LMT_MAPTBL_ENTRY_SIZE;
 }
 
