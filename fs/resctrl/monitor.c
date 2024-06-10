b2577186747a53e821eab213afb14a17961d6fcc
James Morse <james.morse@arm.com>
2024-06-10 17:41:58 +0100

    fs/resctrl: Avoid a race with dom_data_exit() and rmid_ptrs[]
    On MPAM systems if an error occurs the arhictecture code will call
    resctrl_exit(). This calls dom_data_exit() which takes the
    rdrgroup_mutex and kfree()s rmid_ptrs[].
    
    It is possible that another syscall tries to access that same array in
    the meantime, but is blocked on the mutex. Once dom_data_exit()
    completes, that syscall will see a NULL pointer.
    
    Make __rmid_entry() return NULL in this case. Neither __check_limbo()
    nor free_rmid() return an error, and can silently stop their work
    if this occurs. dom_data_init() has only just allocated the array and
    still holds the lock, so __rmid_entry() should never return NULL here.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- fs/resctrl/monitor.c
+++ fs/resctrl/monitor.c
@@ -98,11 +98,16 @@ unsigned int resctrl_rmid_realloc_limit;
  *
  * The domain's rmid_busy_llc and rmid_ptrs[] are sized by index. The arch code
  * must accept an attempt to read every index.
+ *
+ * Returns NULL if the rmid_ptrs[] array is not allocated.
  */
 static inline struct rmid_entry *__rmid_entry(u32 idx)
 {
 	struct rmid_entry *entry;
 	u32 closid, rmid;
+
+	if (!rmid_ptrs)
+		return NULL;
 
 	entry = &rmid_ptrs[idx];
 	resctrl_arch_rmid_idx_decode(idx, &closid, &rmid);
@@ -173,6 +178,8 @@ void __check_limbo(struct rdt_mon_domain *d, bool force_free)
 			break;
 
 		entry = __rmid_entry(idx);
+		if (!entry)
+			break;
 		if (resctrl_arch_rmid_read(r, d, entry->closid, entry->rmid,
 					   QOS_L3_OCCUP_EVENT_ID, &val,
 					   arch_mon_ctx)) {
@@ -353,6 +360,8 @@ void free_rmid(u32 closid, u32 rmid)
 		return;
 
 	entry = __rmid_entry(idx);
+	if (!entry)
+		return;
 
 	if (resctrl_arch_is_llc_occupancy_enabled())
 		add_rmid_to_limbo(entry);
@@ -823,6 +832,7 @@ static int dom_data_init(struct rdt_resource *r)
 	idx = resctrl_arch_rmid_idx_encode(RESCTRL_RESERVED_CLOSID,
 					   RESCTRL_RESERVED_RMID);
 	entry = __rmid_entry(idx);
+	WARN_ON_ONCE(!entry);
 	list_del(&entry->list);
 
 out_unlock:
