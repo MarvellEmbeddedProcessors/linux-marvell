03af943200136ab037d89a28e30b0b765e275f51
Ratheesh Kannoth <rkannoth@marvell.com>
2024-07-11 12:41:43 +0530

    octeontx2-af: npc: cn20k: Use common APIs to alloc/free/get mcam index.
    cn20k uses different algorithm for mcam index management.
    Common APIs should call these index management APIs to alloc,
    free and get default mcam indexes.
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Change-Id: I56b0bf65d5b8714c861ef09ddef15778869ceffa
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/133066
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -1240,8 +1240,8 @@ void rvu_mac_reset(struct rvu *rvu, u16 pcifunc);
 u64 rvu_cgx_get_dmacflt_dropped_pktcnt(void *cgxd, int lmac_id);
 u32 rvu_cgx_get_lmac_fifolen(struct rvu *rvu, int cgx, int lmac);
 void cgx_start_linkup(struct rvu *rvu);
-int npc_get_nixlf_mcam_index(struct npc_mcam *mcam, u16 pcifunc, int nixlf,
-			     int type);
+int npc_get_nixlf_mcam_index(struct rvu *rvu, struct npc_mcam *mcam,
+			     u16 pcifunc, int nixlf, int type);
 bool is_mcam_entry_enabled(struct rvu *rvu, struct npc_mcam *mcam, int blkaddr,
 			   int index);
 int rvu_npc_init(struct rvu *rvu);
