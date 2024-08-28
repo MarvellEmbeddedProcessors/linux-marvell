3be5b5da7d171a5ab12669ee998d6f537bec21e7
Linu Cherian <lcherian@marvell.com>
2024-08-28 14:24:37 +0530

    octeontx2-af: npa: cn20k: Add debugfs support for Halo
    Add debugfs support for NPA halo context.
    
    Sample output on cn20k::
    /sys/kernel/debug/cn20k/npa # ls
    aura_ctx  halo_ctx  pool_ctx  qsize
    
    /sys/kernel/debug/cn20k/npa # cat qsize
    Aura count : 128
    Aura context ena/dis bitmap : 00000000,00000000,00000000,00000003
    
    Halo context ena/dis bitmap : 00000000,00000000,00000000,0000000c
    
    Pool count : 6
    Pool context ena/dis bitmap : 03
    
    /sys/kernel/debug/cn20k/npa # echo 0 1 > halo_ctx
    /sys/kernel/debug/cn20k/npa # cat halo_ctx
    ======halo : 2=======
    W0: Stack base          ffffff790000
    W1: ena                 1
    W1: nat_align           0
    W1: stack_caching       1
    W1: aura drop ena       0
    W1: aura drop           0
    W1: buf_offset          0
    W1: buf_size            32
    W1: ref_cnt_prof                0
    W2: stack_max_pages     13
    W2: stack_pages         11
    W3: bp_0                0
    W3: bp_1                0
    W3: bp_2                0
    
    snip ..
    
    Change-Id: I97f4ca1bf2c0b727d6730d51c8c7a5a4c5623e2c
    Signed-off-by: Linu Cherian <lcherian@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/149171
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cn20k/debugfs.h
+++ drivers/net/ethernet/marvell/octeontx2/af/cn20k/debugfs.h
@@ -24,6 +24,8 @@ void print_npa_cn20k_aura_ctx(struct seq_file *m,
 			      struct npa_cn20k_aq_enq_rsp *rsp);
 void print_npa_cn20k_pool_ctx(struct seq_file *m,
 			      struct npa_cn20k_aq_enq_rsp *rsp);
+void print_npa_cn20k_halo_ctx(struct seq_file *m,
+			      struct npa_aq_enq_rsp *rsp);
 
 int npc_cn20k_debugfs_init(struct rvu *rvu);
 void npc_cn20k_debugfs_deinit(struct rvu *rvu);
