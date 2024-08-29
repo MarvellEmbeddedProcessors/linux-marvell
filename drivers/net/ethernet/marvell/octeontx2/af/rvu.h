854f9174771c77a10d7c8ebf37f5e51e2729c89a
Srujana Challa <schalla@marvell.com>
2024-08-29 13:39:35 +0530

    octeontx2-af: configure default CPT credits for CN10KA B0
    The maximum CPT credits that RXC can use are now configurable on CN10KA B0
    through a hardware CSR. This patch sets the default value to optimize peak
    performance, aligning it with other chip versions.
    
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -682,6 +682,26 @@ static inline bool is_rvu_otx2(struct rvu *rvu)
 		midr == PCI_REVISION_ID_95XXMM || midr == PCI_REVISION_ID_95XXO);
 }
 
+static inline bool is_cn10ka_a0(struct rvu *rvu)
+{
+	struct pci_dev *pdev = rvu->pdev;
+
+	if (pdev->subsystem_device == PCI_SUBSYS_DEVID_CN10K_A &&
+	    (pdev->revision & 0x0F) == 0x0)
+		return true;
+	return false;
+}
+
+static inline bool is_cn10ka_a1(struct rvu *rvu)
+{
+	struct pci_dev *pdev = rvu->pdev;
+
+	if (pdev->subsystem_device == PCI_SUBSYS_DEVID_CN10K_A &&
+	    (pdev->revision & 0x0F) == 0x1)
+		return true;
+	return false;
+}
+
 static inline bool is_cn10kb(struct rvu *rvu)
 {
 	struct pci_dev *pdev = rvu->pdev;
