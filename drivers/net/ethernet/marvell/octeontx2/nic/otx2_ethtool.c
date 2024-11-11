6c6934a1eb0a16528959167614e90247cc733e68
Hariprasad Kelam <hkelam@marvell.com>
2024-11-11 16:50:47 +0530

    octeontx2-af: cn10k: new Interface modes support
    This patch adds new interface modes like single lane
    50G and 25GCR/KR_C consortium modes etc.
    
    'CGX_CMD_MODE_CHANGE' args are changed as below to accomdate
    new modes.
    
    struct cgx_mode_change_args {
            uint64_t reserved1:8;
            uint64_t speed:4;
            uint64_t duplex:1;
            uint64_t an:1;
    	uint64_t port:8;
            uint64_t mode_ID:42;       /* 1 << eth_mode */
    };
    
    As port field is unused in args its modified as below
            uint64_t reserved2:6;
    	uint64_t mode_group_idx:2;
    
    'mode_group_idx' categorize the mode ID range to accommodate more modes.
    To specify mode ID range of 0 - 41, this field will be 0.
    To specify mode ID range of 42 - 83, this field will be 1.
    mode ID will be still mentioned as 1 << (0 - 41). But the mode_group_idx
    decides the actual mode range
    
    minor changes:-
            use linkmode_or for supported/advertised link modes
    such that existing modes(autoneg) wont get overwritten
    
    Change-Id: I4cd21b23a39751b37131a70af298142efd8a1522
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
@@ -1174,7 +1174,8 @@ static void otx2_get_link_mode_info(u64 link_mode_bmap,
 	};
 	u8 bit;
 
-	for_each_set_bit(bit, (unsigned long *)&link_mode_bmap, 27) {
+	for_each_set_bit(bit, (unsigned long *)&link_mode_bmap,
+			 ARRAY_SIZE(cgx_link_mode)) {
 		/* SGMII mode is set */
 		if (bit == 0)
 			linkmode_set_bit_array(otx2_sgmii_features,
@@ -1185,11 +1186,13 @@ static void otx2_get_link_mode_info(u64 link_mode_bmap,
 	}
 
 	if (req_mode == OTX2_MODE_ADVERTISED)
-		linkmode_copy(link_ksettings->link_modes.advertising,
-			      otx2_link_modes);
+		linkmode_or(link_ksettings->link_modes.advertising,
+			    link_ksettings->link_modes.advertising,
+			    otx2_link_modes);
 	else
-		linkmode_copy(link_ksettings->link_modes.supported,
-			      otx2_link_modes);
+		linkmode_or(link_ksettings->link_modes.supported,
+			    link_ksettings->link_modes.supported,
+			    otx2_link_modes);
 }
 
 static int otx2_get_link_ksettings(struct net_device *netdev,
