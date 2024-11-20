c48db6b617d816d7a7159a81b398ebeec985d2a5
Suman Ghosh <sumang@marvell.com>
2024-11-20 11:11:52 +0530

    octeontx2-pf: Fix module compilation failure
    Fixes: 21205723cbe3 ("octeontx2-pf: Add ucast filter count configurability via devlink")
    
    Change-Id: I6286a7eaa983f55fa9e6f14a129b0968bc32030b
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Suman Ghosh <sumang@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_flows.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_flows.c
@@ -257,6 +257,7 @@ int otx2_mcam_entry_init(struct otx2_nic *pfvf)
 	refcount_set(&flow_cfg->mark_flows, 1);
 	return 0;
 }
+EXPORT_SYMBOL(otx2_mcam_entry_init);
 
 /* TODO : revisit on size */
 #define OTX2_DMAC_FLTR_BITMAP_SZ (4 * 2048 + 32)
