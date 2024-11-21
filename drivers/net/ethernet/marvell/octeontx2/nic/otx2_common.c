61dd5be8d606f56077485cf494ee587b0f7e5568
Suman Ghosh <sumang@marvell.com>
2024-11-21 14:24:30 +0530

    octeontx2-pf: Add correct page pool parameters
    The change to use correct page pool parameters are part of CN20K patch.
    Since as part of this 6.1 to 6.6 porting, we are not porting the cn20k
    changes, this patch is a temporary fix to enable PF/VF and workings of
    networkings.
    
    Signed-off-by: Suman Ghosh <sumang@marvell.com>
    Change-Id: I1378ba527b8768af988a1a811bebdbe56d3bd042
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/139726
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.c
@@ -1391,7 +1391,7 @@ int otx2_pool_init(struct otx2_nic *pfvf, u16 pool_id,
 	struct page_pool_params pp_params = { 0 };
 	struct npa_aq_enq_req *aq;
 	struct otx2_pool *pool;
-	int err;
+	int err, sz;
 
 	pool = &pfvf->qset.pool[pool_id];
 	/* Alloc memory for stack which is used to store buffer pointers */
@@ -1437,8 +1437,9 @@ int otx2_pool_init(struct otx2_nic *pfvf, u16 pool_id,
 		return 0;
 	}
 
-	pp_params.order = get_order(buf_size);
-	pp_params.flags = PP_FLAG_DMA_MAP;
+	sz = ALIGN(ALIGN(SKB_DATA_ALIGN(buf_size), OTX2_ALIGN), PAGE_SIZE);
+	pp_params.order = (sz / PAGE_SIZE) - 1;
+	pp_params.flags = PP_FLAG_PAGE_FRAG | PP_FLAG_DMA_MAP;
 	pp_params.pool_size = min(OTX2_PAGE_POOL_SZ, numptrs);
 	pp_params.nid = NUMA_NO_NODE;
 	pp_params.dev = pfvf->dev;
