b677ed2bfe55ae2e0cc4d3f57c726880ce502c6c
Bharat Bhushan <bbhushan2@marvell.com>
2025-01-28 13:58:53 +0530

    cn10k-ipsec: Process outbound ipsec crypto offload
    Prepare and submit crypto hardware (CPT) instruction for
    outbound ipsec crypto offload. The CPT instruction have
    authentication offset, IV offset and encapsulation offset
    in input packet. Also provide SA context pointer which have
    details about algo, keys, salt etc. Crypto hardware encrypt,
    authenticate and provide the ESP packet to networking hardware.
    
    Change-Id: I8e00c49e4d0293406b723cf5afe706ac179a01b7
    Signed-off-by: Bharat Bhushan <bbhushan2@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/144210
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.h
@@ -116,6 +116,9 @@ struct otx2_snd_queue {
 	u16			sqb_count;
 	u64			*sqb_ptrs;
 	struct xsk_buff_pool    *xsk_pool;
+	/* SQE ring and CPT response queue for Inline IPSEC */
+	struct qmem		*sqe_ring;
+	struct qmem		*cpt_resp;
 } ____cacheline_aligned_in_smp;
 
 enum cq_type {
