621e7a3a2dfb2e01b271d53f94da041c08561db8
Wilson Ding <dingwei@marvell.com>
2025-02-06 14:54:00 -0800

    PCI: armada8k: Add support for device reset
    Support issuing #PERST via GPIO as described in pci.txt dt-binding
    document.
    
    Signed-off-by: Wilson Ding <dingwei@marvell.com>
    Change-Id: Ic67b6f97cb4ede2b18be4d40c7ff63795bfeac78
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/165184
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/pci/controller/dwc/pcie-armada8k.c
+++ drivers/pci/controller/dwc/pcie-armada8k.c
@@ -13,14 +13,16 @@
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
+#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/of.h>
+#include <linux/of_pci.h>
 #include <linux/pci.h>
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
 #include <linux/resource.h>
-#include <linux/of_pci.h>
 
 #include "pcie-designware.h"
 
@@ -32,6 +34,7 @@ struct armada8k_pcie {
 	struct clk *clk_reg;
 	struct phy *phy[ARMADA8K_PCIE_MAX_LANES];
 	unsigned int phy_count;
+	struct gpio_desc *reset_gpio;
 };
 
 #define PCIE_VENDOR_REGS_OFFSET		0x8000
@@ -171,10 +174,27 @@ static int armada8k_pcie_host_init(struct dw_pcie_rp *pp)
 	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
 
 	if (!dw_pcie_link_up(pci)) {
+		struct armada8k_pcie *pcie = to_armada8k_pcie(pci);
+
 		/* Disable LTSSM state machine to enable configuration */
 		reg = dw_pcie_readl_dbi(pci, PCIE_GLOBAL_CONTROL_REG);
 		reg &= ~(PCIE_APP_LTSSM_EN);
 		dw_pcie_writel_dbi(pci, PCIE_GLOBAL_CONTROL_REG, reg);
+
+		if (pcie->reset_gpio) {
+			dev_dbg(pci->dev, "resetting device via gpio\n");
+
+			/* Assert #PERST */
+			gpiod_set_value_cansleep(pcie->reset_gpio, 1);
+			/*
+			* Ensure that PERST has been asserted for at least 100
+			* ms. Section 2.2 of PCI Express Card Electromechanical
+			* Specification Revision 3.0
+			*/
+			msleep(100);
+			/* Deassert #PERST */
+			gpiod_set_value_cansleep(pcie->reset_gpio, 0);
+		}
 	}
 
 	/* Set the device to root complex mode */
@@ -301,6 +321,12 @@ static int armada8k_pcie_probe(struct platform_device *pdev)
 		ret = clk_prepare_enable(pcie->clk_reg);
 		if (ret)
 			goto fail_clkreg;
+	}
+
+	pcie->reset_gpio = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_LOW);
+	if (pcie->reset_gpio == ERR_PTR(-EPROBE_DEFER)) {
+		ret = -EPROBE_DEFER;
+		goto fail_clkreg;
 	}
 
 	/* Get the dw-pcie unit configuration/control registers base. */
