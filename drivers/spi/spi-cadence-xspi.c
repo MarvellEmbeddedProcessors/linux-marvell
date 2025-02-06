91bb4fcefad68d73187e19a0f52c7c6640dd4001
Dhananjay Kangude <dkangude@marvell.com>
2025-02-06 02:34:04 -0800

    xspi: replaced mdelay with msleep
    msleep is better option as it allows CPU to switch to other processes
    instead of activly waiting like mdelay hence system could manage
    resource better with msleep.
    
    Signed-off-by: Dhananjay Kangude <dkangude@marvell.com>
    Change-Id: Icd8c2fb4a85bf59b6c78b767086b90db7bbf7f44
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/145185
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/spi/spi-cadence-xspi.c
+++ drivers/spi/spi-cadence-xspi.c
@@ -431,7 +431,7 @@ static int lock_spi_bus(struct cdns_xspi_dev *cdns_xspi)
 	if (cdns_xspi->lockbase) {
 		while (timeout-- >= 0) {
 			if (xspi_trylock(&lock->lock) != 0) {
-				mdelay(SPI_LOCK_SLEEP_DURATION_MS);
+				msleep(SPI_LOCK_SLEEP_DURATION_MS);
 				continue;
 			}
 			if (lock->owner == 0 || lock->owner == SPI_AP_NS_OWN) {
@@ -440,7 +440,7 @@ static int lock_spi_bus(struct cdns_xspi_dev *cdns_xspi)
 				return 0;
 			}
 			xspi_unlock(&lock->lock);
-			mdelay(SPI_LOCK_SLEEP_DURATION_MS);
+			msleep(SPI_LOCK_SLEEP_DURATION_MS);
 			continue;
 		}
 		if (lock->owner > 2) {
@@ -469,7 +469,7 @@ static int lock_spi_bus(struct cdns_xspi_dev *cdns_xspi)
 			if (check == 0)
 				return 0; // got a lock
 		}
-		mdelay(SPI_LOCK_SLEEP_DURATION_MS);
+		msleep(SPI_LOCK_SLEEP_DURATION_MS);
 	}
 	pr_err("Flash arbitration failed, lock is owned by: %d\n", val);
 	return -1;
