26dc3ece0daa17e45b786c5dd03767f142e984e1
Srujana Challa <schalla@marvell.com>
2025-04-23 15:31:45 +0530

    cn20k-af: psw: add mailbox to get PSW capabilities
    This patch introduces a new mailbox to obtain PSW capabilities,
    including PSW_AF_CONST0, PSW_AF_CONST1, PSW_AF_CONST2, and
    PSW_AF_FID_TYPEX_CONST.
    
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Change-Id: If00d7f6579cfbd07306ddb771e49530f478f2775
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/151515
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/soc/marvell/rvu_af_extn/rvu_psw_mbox.h
+++ drivers/soc/marvell/rvu_af_extn/rvu_psw_mbox.h
@@ -16,6 +16,7 @@ M(PSW_FREE_RSRC_CNT,	0x1202, psw_free_rsrc_cnt, msg_req,             \
 				psw_free_rsrcs_rsp)                     \
 M(PSW_MSIX_OFFSET,	0x1203, psw_msix_offset, msg_req,               \
 				psw_msix_offset_rsp)                    \
+M(PSW_CAPS_GET,      0x1204, psw_caps_get, msg_req, psw_caps_get_rsp)	\
 
 enum {
 #define M(_name, _id, _1, _2, _3) MBOX_MSG_ ## _name = _id,
@@ -53,4 +54,28 @@ struct psw_msix_offset_rsp {
 	u16 pswlfs;
 	u16 pswlf_msixoff[MAX_RVU_BLKLF_CNT];
 };
+
+struct psw_caps_get_rsp {
+	struct mbox_msghdr hdr;
+	/* Host pf func ID of requested RVU pcifunc */
+	u8 epf;
+	u8 rsvd[7];
+	/* PSW_AF_CONST0 includes, mevf, mdbl, mmsix and nepf */
+	u64 const0;
+	/* PSW_AF_CONST1 includes, mqueues, fidentrynum, gidbucketnum
+	 * and gidentrynum
+	 */
+	u64 const1;
+	/* PSW_AF_CONST2 includes, shared_size, TPT entries, PST and TST
+	 * entries and number of LFs.
+	 */
+	u64 const2;
+#define PSW_TYPE_COUNT 9
+	/* PSW_AF_FID_TYPE(0..8)_CONST, includes pfoffset and vfoffset
+	 * for each psw type
+	 */
+	u64 fid_type_const[PSW_TYPE_COUNT];
+	u64 rsvd1[7];
+};
+
 #endif /* __RVU_PSW_MBOX_H__ */
