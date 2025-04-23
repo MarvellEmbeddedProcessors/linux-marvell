651cd3a2e69996206a63f3b98742b5c712c350e5
Srujana Challa <schalla@marvell.com>
2025-04-23 17:34:01 +0530

    cn20k-af: psw: add mailbox for PSW PCIe configuration
    Introduces a new mailbox to configure the PSW PCIe settings
    for both EPF and EVF.
    
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Change-Id: Ic9afef58a12d2582a3200b79c07e2ff0c3e15ae5
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/151520
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/soc/marvell/rvu_af_extn/rvu_psw_mbox.h
+++ drivers/soc/marvell/rvu_af_extn/rvu_psw_mbox.h
@@ -26,6 +26,8 @@ M(PSW_FID_FREE_ENTRY,   0x1208, psw_fid_free_entry, psw_fid_free_entry_req,\
 M(PSW_EPF_DBL_CFG,      0x1209, psw_epf_dbl_cfg, psw_epf_dbl_cfg_req, msg_rsp) \
 M(PSW_EPFVF_MAP_CFG,    0x120A, psw_epfvf_map_cfg, psw_epfvf_map_cfg_req, \
 				msg_rsp)				\
+M(PSW_EPFVF_PCIE_CFG,   0x120B, psw_epfvf_pcie_cfg, psw_epfvf_pcie_cfg_req, \
+				msg_rsp)				\
 
 enum {
 #define M(_name, _id, _1, _2, _3) MBOX_MSG_ ## _name = _id,
@@ -184,6 +186,18 @@ struct psw_epfvf_map_cfg_req {
 	u16 lf_id; /* PSW LF slot id */
 	u8 enable; /* Enable or disable EPF/EVF mapping */
 	u8 rsvd1[3];
+	u64 rsvd2;
+};
+
+/* Configures PSW_AF_EPF(0..15)_EVF(0..127)_PCIE_CFG and
+ * PSW_AF_EPF(0..15)_PCIE_CFG
+ */
+struct psw_epfvf_pcie_cfg_req {
+	struct mbox_msghdr hdr;
+	u16 evf_id; /* Host VF ID */
+	u8 msix_enable; /* Enable or disable MSIX */
+	u8 master_enable; /* Enable or disable master */
+	u32 rsvd1;
 	u64 rsvd2;
 };
 
