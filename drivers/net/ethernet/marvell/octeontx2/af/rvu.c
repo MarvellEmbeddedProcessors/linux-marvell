287f5245e84ac595b5fe72515bc8a41c8083d410
Srujana Challa <schalla@marvell.com>
2025-04-23 12:00:14 +0530

    cn20k-af: psw: introduce PSW block
    The PCIe Software Notification(PSW) unit is a hardware accelerator
    introduced in OCTEON CN20K chip version. This versatile accelerator
    enables the device to support various protocols such as NVMe, VirtIO
    and SDXI.
    This patch introduces PSW register definitions and includes the basic
    initialization of the PSW block as an extension block in AF.
    
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Change-Id: I605fb27419d2db8f1689d8aeffd544d0e5e9d1b3
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/151513
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -315,6 +315,9 @@ int rvu_get_blkaddr(struct rvu *rvu, int blktype, u16 pcifunc)
 	case BLKTYPE_ML:
 		blkaddr = BLKADDR_ML;
 		goto exit;
+	case BLKTYPE_PSW:
+		blkaddr = BLKADDR_PSW;
+		goto exit;
 	}
 
 	/* Check if this is a RVU PF or VF */
@@ -469,6 +472,9 @@ void rvu_update_rsrc_map(struct rvu *rvu, struct rvu_pfvf *pfvf,
 	case BLKADDR_ML:
 		attach ? pfvf->mllfs++ : pfvf->mllfs--;
 		num_lfs = pfvf->mllfs;
+		break;
+	case BLKADDR_PSW:
+		attach ? pfvf->pswlfs++ : pfvf->pswlfs--;
 		break;
 	}
 
@@ -1446,6 +1452,8 @@ u16 rvu_get_rsrc_mapcount(struct rvu_pfvf *pfvf, int blkaddr)
 		return pfvf->dpi1_lfs;
 	case BLKADDR_ML:
 		return pfvf->mllfs;
+	case BLKADDR_PSW:
+		return pfvf->pswlfs;
 	}
 	return 0;
 }
