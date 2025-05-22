b68c8994dff789e9b53b41a0780f67f670471ed8
Geetha sowjanya <gakula@marvell.com>
2025-05-22 16:18:00 +0530

    octeontx2-af: cn20k: mcs set mcs id and silicon check
    Set the MCS id based on number of mcs instance enumerated.
    Current code uses mcs_blk_cnt to differentiate between cn10kb
    and cnf10kb. This patch replaces those check with mcs_devtype
    which is set based on the silicon variant.
    
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Change-Id: I3001d733aff0a3effcd93b7e4bc41199b187b7d9
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/154179
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_debugfs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_debugfs.c
@@ -20,6 +20,7 @@
 #include "npc.h"
 #include "rvu_npc_hash.h"
 #include "mcs.h"
+#include "mcs_reg.h"
 
 #include "cn20k/debugfs.h"
 
@@ -213,7 +214,7 @@ static int rvu_dbg_mcs_port_stats_display(struct seq_file *filp, void *unused, i
 		seq_printf(filp, "port%d: Tcam Miss: %lld\n", lmac, stats.tcam_miss_cnt);
 		seq_printf(filp, "port%d: Parser errors: %lld\n", lmac, stats.parser_err_cnt);
 
-		if (dir == MCS_RX && mcs->hw->mcs_blks > 1)
+		if (dir == MCS_RX && mcs->hw->mcs_devtype == CNF10KB_MCS)
 			seq_printf(filp, "port%d: Preempt error: %lld\n", lmac,
 				   stats.preempt_err_cnt);
 		if (dir == MCS_TX)
