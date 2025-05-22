791a50a6c1e0eff4014caccb85f7202bc9f0013d
James Morse <james.morse@arm.com>
2025-05-22 16:53:44 +0100

    arm64: mpam: Add helpers to change a tasks and cpu mpam partid/pmg values
    Care must be taken when modifying the PARTID and PMG of a task, as
    writing these values may race with the task being scheduled in, and
    reading the modified values.
    
    Add helpers to set the task properties, and the CPU default value.
    These use WRITE_ONCE() that pairs with the READ_ONCE() in mpam_get_regval()
    to avoid causing torn values.
    
    CC: Dave Martin <Dave.Martin@arm.com>
    CC: Ben Horgan <ben.horgan@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/arm64/include/asm/mpam.h
+++ arch/arm64/include/asm/mpam.h
@@ -5,6 +5,7 @@
 #define __ASM__MPAM_H
 
 #include <linux/bitops.h>
+#include <linux/bitfield.h>
 #include <linux/init.h>
 #include <linux/jump_label.h>
 #include <linux/percpu.h>
@@ -29,6 +30,35 @@ DECLARE_PER_CPU(u64, arm64_mpam_current);
  * A value in struct thread_info is used instead of struct task_struct as the
  * cpu's u64 register format is used, but struct task_struct has two u32'.
  */
+static inline void mpam_set_cpu_defaults(int cpu, u16 partid_d, u16 partid_i,
+					 u8 pmg_d, u8 pmg_i)
+{
+	u64 default_val;
+
+	default_val = FIELD_PREP(MPAM0_EL1_PARTID_D, partid_d);
+	default_val |= FIELD_PREP(MPAM0_EL1_PARTID_I, partid_i);
+	default_val |= FIELD_PREP(MPAM0_EL1_PMG_D, pmg_d);
+	default_val |= FIELD_PREP(MPAM0_EL1_PMG_I, pmg_i);
+
+	WRITE_ONCE(per_cpu(arm64_mpam_default, cpu), default_val);
+}
+
+static inline void mpam_set_task_partid_pmg(struct task_struct *tsk,
+					    u16 partid_d, u16 partid_i,
+					    u8 pmg_d, u8 pmg_i)
+{
+#ifdef CONFIG_ARM64_MPAM
+	u64 regval;
+
+	regval = FIELD_PREP(MPAM0_EL1_PARTID_D, partid_d);
+	regval |= FIELD_PREP(MPAM0_EL1_PARTID_I, partid_i);
+	regval |= FIELD_PREP(MPAM0_EL1_PMG_D, pmg_d);
+	regval |= FIELD_PREP(MPAM0_EL1_PMG_I, pmg_i);
+
+	WRITE_ONCE(task_thread_info(tsk)->mpam_partid_pmg, regval);
+#endif
+}
+
 static inline u64 mpam_get_regval(struct task_struct *tsk)
 {
 #ifdef CONFIG_ARM64_MPAM
