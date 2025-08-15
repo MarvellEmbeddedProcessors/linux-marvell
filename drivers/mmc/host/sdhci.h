89ead592c564b08a91d4e3cdd91cacec9aa672d9
Jisheng Zhang <jszhang@kernel.org>
2025-08-15 09:33:36 +0800

    mmc: sdhci: add some simple inline functions for !CONFIG_PM
    In next commits, we will switch to the modern PM macros.
    
    Change-Id: I39aeb7d81e20b652253dbc7a32ccaee6a2dad5c4
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Link: https://lore.kernel.org/r/20250815013413.28641-2-jszhang@kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Fixes: 18be7f779657 ("mmc: sdhci-cadence: use modern PM macros")

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -806,6 +806,13 @@ int sdhci_suspend_host(struct sdhci_host *host);
 int sdhci_resume_host(struct sdhci_host *host);
 int sdhci_runtime_suspend_host(struct sdhci_host *host);
 int sdhci_runtime_resume_host(struct sdhci_host *host, int soft_reset);
+#else
+static inline bool sdhci_enable_irq_wakeups(struct sdhci_host *host) { return false; }
+static inline void sdhci_disable_irq_wakeups(struct sdhci_host *host) {}
+static inline int sdhci_suspend_host(struct sdhci_host *host) { return -EOPNOTSUPP; }
+static inline int sdhci_resume_host(struct sdhci_host *host) { return -EOPNOTSUPP; }
+static inline int sdhci_runtime_suspend_host(struct sdhci_host *host) { return 0; }
+static inline int sdhci_runtime_resume_host(struct sdhci_host *host, int soft_reset) { return 0; }
 #endif
 
 void sdhci_cqe_enable(struct mmc_host *mmc);
