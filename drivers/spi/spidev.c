e8a0bc9b93ea2397e9130271410787e801dc2634
Bharat Bhushan <bbhushan2@marvell.com>
2025-08-29 14:19:46 +0530

    spidev: Add Renesas rc35312 device compatible
    Add compatible string for Renesas rc35312 device.
    
    Change-Id: Ib30c6cad8cbb58b09e3aa9cdef45731a4dffe8ac
    Signed-off-by: Bharat Bhushan <bbhushan2@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/161547
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -715,6 +715,7 @@ static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = "spi-authenta" },
 	{ .name = "em3581" },
 	{ .name = "si3210" },
+	{ .name = "rc35312" },
 	{},
 };
 MODULE_DEVICE_TABLE(spi, spidev_spi_ids);
@@ -744,6 +745,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "semtech,sx1301", .data = &spidev_of_check },
 	{ .compatible = "silabs,em3581", .data = &spidev_of_check },
 	{ .compatible = "silabs,si3210", .data = &spidev_of_check },
+	{ .compatible = "renesas,rc35312", .data = &spidev_of_check },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
