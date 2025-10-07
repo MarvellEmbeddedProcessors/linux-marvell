3841d7e85f34406702623b2ae62a97a25666d3c7
Naveen Mamindlapalli <naveenm@marvell.com>
2025-10-07 09:39:03 +0530

    octeontx2-pf: restart interface when link settings are changed by user
    When user changes link settings (such as speed, duplex, or autoneg),
    the driver now invokes ndo_close() followed by ndo_open() to ensure
    the interface is properly reinitialized.
    
    This sequence guarantees that no traffic flows during reconfiguration
    and prevents potential NIX hang conditions observed when link settings
    are updated while the interface remains active.
    
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Change-Id: Idfe0b83a8680d7dbb7bb60892c278a1d08164e6c
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/164242
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
@@ -1360,6 +1360,7 @@ static int otx2_set_link_ksettings(struct net_device *netdev,
 	struct cgx_set_link_mode_rsp *rsp;
 	struct mbox *mbox = &pf->mbox;
 	int err = 0;
+	bool if_up;
 
 	memset(&cur_ks, 0, sizeof(struct ethtool_link_ksettings));
 
@@ -1377,6 +1378,10 @@ static int otx2_set_link_ksettings(struct net_device *netdev,
 	if (!linkmode_subset(cmd->link_modes.advertising,
 			     cur_ks.link_modes.supported))
 		return -EINVAL;
+
+	if_up = netif_running(netdev);
+	if (if_up)
+		netdev->netdev_ops->ndo_stop(netdev);
 
 	mutex_lock(&mbox->lock);
 	req = otx2_mbox_alloc_msg_cgx_set_link_mode(&pf->mbox);
@@ -1414,6 +1419,8 @@ static int otx2_set_link_ksettings(struct net_device *netdev,
 	}
 end:
 	mutex_unlock(&mbox->lock);
+	if (if_up)
+		netdev->netdev_ops->ndo_open(netdev);
 	return err;
 }
 
