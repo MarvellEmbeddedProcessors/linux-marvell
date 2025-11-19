e6306ea2e78102e69e30d767d3c7a4ed012a780d
James Morse <james.morse@arm.com>
2025-11-19 12:22:34 +0000

    ACPI / PPTT: Find cache level by cache-id
    The MPAM table identifies caches by id. The MPAM driver also wants to know
    the cache level to determine if the platform is of the shape that can be
    managed via resctrl. Cacheinfo has this information, but only for CPUs that
    are online.
    
    Waiting for all CPUs to come online is a problem for platforms where
    CPUs are brought online late by user-space.
    
    Add a helper that walks every possible cache, until it finds the one
    identified by cache-id, then return the level.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Ben Horgan <ben.horgan@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Jeremy Linton <jeremy.linton@arm.com>
    Tested-by: Fenghua Yu <fenghuay@nvidia.com>
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com>
    Tested-by: Gavin Shan <gshan@redhat.com>
    Tested-by: Zeng Heng <zengheng4@huawei.com>
    Tested-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1511,6 +1511,7 @@ int find_acpi_cpu_topology_cluster(unsigned int cpu);
 int find_acpi_cpu_topology_package(unsigned int cpu);
 int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
 void acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus);
+int find_acpi_cache_level_from_id(u32 cache_id);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1534,6 +1535,10 @@ static inline int find_acpi_cpu_topology_hetero_id(unsigned int cpu)
 }
 static inline void acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id,
 						     cpumask_t *cpus) { }
+static inline int find_acpi_cache_level_from_id(u32 cache_id)
+{
+	return -ENOENT;
+}
 #endif
 
 #ifdef CONFIG_ARM64
