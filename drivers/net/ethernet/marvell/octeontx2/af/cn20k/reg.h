8c03c2e6b9d29b681aba89eccfabe0d6b97aadc7
Subbaraya Sundeep <sbhatta@marvell.com>
2026-01-30 18:17:00 +0530

    octeontx2: sdp: Display backpressure status in debugfs
    When NIX TX sends packets to SDP and if SDP run out of host buffers
    then SDP hardware can assert backpressure to NIX. This patch displays
    SDP hardware backpressure status for the rings alloted to host.
    
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cn20k/reg.h
+++ drivers/net/ethernet/marvell/octeontx2/af/cn20k/reg.h
@@ -227,5 +227,10 @@ static inline u64 pf_to_bitoff(u8 pf)
 #define SDP_AF_RX_OUT_DROP_PKT_CNT(a)		(0x8000580 | (a) << 16)
 #define SDP_AF_RX_OUT_DROP_BYTE_CNT(a)		(0x8000590 | (a) << 16)
 #define SDP_AF_RX_OUT_PTP_STATS(a)		(0x8000710 | (a) << 16)
+#define SDP_AF_RX_OUT_SLIST_DBELL(a)		(0x8000120 | (a) << 16)
+#define SDP_AF_RX_OUT_WMARK(a)			(0x8000128 | (a) << 16)
+
+#define SDP_AF_OUT_BP_ENX_W1S(a)		(0x4093040 | (a) << 3)
+#define SDP_AF_OUT_DROP_STATEX(a)		(0x4093080 | (a) << 3)
 
 #endif /* RVU_MBOX_REG_H */
