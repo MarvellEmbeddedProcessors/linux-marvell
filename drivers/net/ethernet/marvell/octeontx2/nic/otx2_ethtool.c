a266ec99271821102392c9cb18a75d796eb10dd6
pkaity <pkaity@marvell.com>
2026-06-22 18:45:49 +0000

    CN10K: Fix for kernel is not reporting 10/100 Mbps SGMII capabilities
    Earlier for SGMII it was checking whether the 10M,
    100M and 1000M bits are set or not in link_mode_bmap,
    if set then only it will display 10M,100M and 1000M
    will be displayed in the supported modes. Modified
    that change so that if SGMII is supported it will
    display all 10M, 100M and 1000M in the supported modes.
    
    Signed-off-by: pkaity <pkaity@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
@@ -1227,6 +1227,14 @@ static void otx2_get_link_mode_info(u64 link_mode_bmap,
 				    *link_ksettings)
 {
 	__ETHTOOL_DECLARE_LINK_MODE_MASK(otx2_link_modes) = { 0, };
+	const int otx2_sgmii_features[6] = {
+	ETHTOOL_LINK_MODE_10baseT_Half_BIT,
+	ETHTOOL_LINK_MODE_10baseT_Full_BIT,
+	ETHTOOL_LINK_MODE_100baseT_Half_BIT,
+	ETHTOOL_LINK_MODE_100baseT_Full_BIT,
+	ETHTOOL_LINK_MODE_1000baseT_Half_BIT,
+	ETHTOOL_LINK_MODE_1000baseT_Full_BIT,
+	};
 	/* CGX link modes to Ethtool link mode mapping */
 	const int cgx_link_mode[CGX_MODE_MAX] = {
 		0, /* SGMII  Mode */
@@ -1293,18 +1301,14 @@ static void otx2_get_link_mode_info(u64 link_mode_bmap,
 
 	for_each_set_bit(bit, (unsigned long *)&link_mode_bmap,
 			 ARRAY_SIZE(cgx_link_mode)) {
-		if (bit == ETH_MODE_SGMII_10M_BIT) {
-			linkmode_set_bit(ETHTOOL_LINK_MODE_10baseT_Half_BIT, otx2_link_modes);
-			linkmode_set_bit(ETHTOOL_LINK_MODE_10baseT_Full_BIT, otx2_link_modes);
-		} else if (bit == ETH_MODE_SGMII_100M_BIT) {
-			linkmode_set_bit(ETHTOOL_LINK_MODE_100baseT_Half_BIT, otx2_link_modes);
-			linkmode_set_bit(ETHTOOL_LINK_MODE_100baseT_Full_BIT, otx2_link_modes);
-		} else if (bit == CGX_MODE_SGMII) {
-			linkmode_set_bit(ETHTOOL_LINK_MODE_1000baseT_Half_BIT, otx2_link_modes);
-			linkmode_set_bit(ETHTOOL_LINK_MODE_1000baseT_Full_BIT, otx2_link_modes);
-		} else {
+		/* SGMII mode is set */
+		if (bit == 0)
+		{
+			linkmode_set_bit_array(otx2_sgmii_features,
+				        ARRAY_SIZE(otx2_sgmii_features),
+				                        otx2_link_modes);
+		} else
 			linkmode_set_bit(cgx_link_mode[bit], otx2_link_modes);
-		}
 	}
 
 	if (req_mode == OTX2_MODE_ADVERTISED)
